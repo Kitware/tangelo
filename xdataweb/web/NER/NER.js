1293f05e58bb7d5da9c88c1baff45b8eb4b17e7a
Roni Choudhury <roni.choudhury@kitware.com>
2012-11-01 18:06:50 -0400

    added file upload capability

--- xdataweb/web/NER/NER.js
+++ xdataweb/web/NER/NER.js
@@ -3,10 +3,21 @@ function handleFileSelect(evt){
     
     var output = [];
     for(var i=0; i<files.length; i++){
-        f = files[i];
-        output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-                f.size, ' bytes, last modified: ',
-                f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a');
+        var f = files[i];
+        output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ', f.size, ' bytes ', f.type == 'text/plain' ? '<span class=ok>(ok)</span>' : '<span class="rejected">(rejected)</span>');
+
+        var reader = new FileReader();
+        reader.onload = (function(file){
+            return function(e){
+                console.log(file.name);
+                var blobs = document.getElementById("blobs");
+                var elem = document.createElement("li");
+                elem.innerHTML = escape(file.name) + ": " + e.target.result.slice(0,40) + "...";
+                blobs.appendChild(elem);
+            }
+        })(f);
+
+        reader.readAsText(f);
     }
     document.getElementById('file-info').innerHTML = '<ul>' + output.join('') + '</ul>';
 }
