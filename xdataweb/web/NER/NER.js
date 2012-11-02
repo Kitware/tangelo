d9c82e15529484ce279c61803f5f631ba0ebca55
Roni Choudhury <roni.choudhury@kitware.com>
2012-11-02 13:57:24 -0400

    put in ajax call

--- xdataweb/web/NER/NER.js
+++ xdataweb/web/NER/NER.js
@@ -4,16 +4,51 @@ function handleFileSelect(evt){
     var output = [];
     for(var i=0; i<files.length; i++){
         var f = files[i];
+        var using = false;
+        if(f.type == '(n/a)'){
+            status = "accepted";
+            msg = "ok, assuming text";
+        }
+        else if(f.type.slice(0,5) == 'text/'){
+            status = "accepted";
+            msg = "ok";
+        }
+        else{
+            status = "rejected";
+            msg = "rejected";
+        }
         output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ', f.size, ' bytes ', f.type == 'text/plain' ? '<span class=ok>(ok)</span>' : '<span class="rejected">(rejected)</span>');
 
         var reader = new FileReader();
         reader.onload = (function(file){
             return function(e){
                 console.log(file.name);
-                var blobs = document.getElementById("blobs");
+
+                // Grab the text of the file.
+                var text = e.target.result;
+
+                // Create a "preview" bullet point containing a prefix of the file text.
                 var elem = document.createElement("li");
-                elem.innerHTML = escape(file.name) + ": " + e.target.result.slice(0,40) + "...";
-                blobs.appendChild(elem);
+                elem.innerHTML = escape(file.name) + ": " + text.slice(0,40) + "...";
+                $("#blobs").get(0).appendChild(elem);
+
+                // Fire an AJAX call to retrieve the named entities in the document.
+                //
+                // TODO(choudhury): remember to URI encode the text!!
+                $.ajax({
+                    url: '/service/echo',
+                    data: {
+                        text: text,
+                        num: 8
+                    },
+                    dataType: 'text',
+                    success: function(data){
+                        console.log("success for " + file.name + " - result: " + data);
+                    },
+                    error: function(){
+                        console.log("error for " + file.name);
+                    }
+                });
             }
         })(f);
 
