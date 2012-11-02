a45f149b2de8dbdb9a993de9ec18d276acb21e7b
Roni Choudhury <roni.choudhury@kitware.com>
2012-11-02 16:14:10 -0400

    changed AJAX call to use NER service, and excluded selected non-text files from being processed

--- xdataweb/web/NER/NER.js
+++ xdataweb/web/NER/NER.js
@@ -4,55 +4,57 @@ function handleFileSelect(evt){
     var output = [];
     for(var i=0; i<files.length; i++){
         var f = files[i];
-        var using = false;
+        var using = null;
         if(f.type == '(n/a)'){
             status = "accepted";
             msg = "ok, assuming text";
+            using = true;
         }
         else if(f.type.slice(0,5) == 'text/'){
             status = "accepted";
             msg = "ok";
+            using = true;
         }
         else{
             status = "rejected";
             msg = "rejected";
+            using = false;
         }
         output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ', f.size, ' bytes ', f.type == 'text/plain' ? '<span class=ok>(ok)</span>' : '<span class="rejected">(rejected)</span>');
 
-        var reader = new FileReader();
-        reader.onload = (function(file){
-            return function(e){
-                console.log(file.name);
+        if(using){
+            var reader = new FileReader();
+            reader.onload = (function(file){
+                return function(e){
+                    console.log(file.name);
 
-                // Grab the text of the file.
-                var text = e.target.result;
+                    // Grab the text of the file.
+                    var text = e.target.result;
 
-                // Create a "preview" bullet point containing a prefix of the file text.
-                var elem = document.createElement("li");
-                elem.innerHTML = escape(file.name) + ": " + text.slice(0,40) + "...";
-                $("#blobs").get(0).appendChild(elem);
+                    // Create a "preview" bullet point containing a prefix of the file text.
+                    var elem = document.createElement("li");
+                    elem.innerHTML = escape(file.name) + ": " + text.slice(0,40) + "...";
+                    $("#blobs").get(0).appendChild(elem);
 
-                // Fire an AJAX call to retrieve the named entities in the document.
-                //
-                // TODO(choudhury): remember to URI encode the text!!
-                $.ajax({
-                    url: '/service/echo',
-                    data: {
-                        text: text,
-                        num: 8
-                    },
-                    dataType: 'text',
-                    success: function(data){
-                        console.log("success for " + file.name + " - result: " + data);
-                    },
-                    error: function(){
-                        console.log("error for " + file.name);
-                    }
-                });
-            }
-        })(f);
+                    // Fire an AJAX call to retrieve the named entities in the document.
+                    $.ajax({
+                        url: '/service/NER',
+                        data: {
+                            text: text
+                        },
+                        dataType: 'text',
+                        success: function(data){
+                            console.log("success for " + file.name + " - result: " + data);
+                        },
+                        error: function(){
+                            console.log("error for " + file.name);
+                        }
+                    });
+                }
+            })(f);
 
-        reader.readAsText(f);
+            reader.readAsText(f);
+        }
     }
     document.getElementById('file-info').innerHTML = '<ul>' + output.join('') + '</ul>';
 }
