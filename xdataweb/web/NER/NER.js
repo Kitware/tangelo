47b6a21ac1218e18f650b70f12d99ec1ccba53e8
Roni Choudhury <roni.choudhury@kitware.com>
2012-11-04 17:40:07 -0500

    got basic graphing working - line link css is missing

--- xdataweb/web/NER/NER.js
+++ xdataweb/web/NER/NER.js
@@ -49,8 +49,52 @@ function assembleGraph(){
 }
 
 function renderGraph(g){
-    //console.log("renderGraph(): g = " + g.nodes.join(",") + g.links.join(","));
-    console.log("renderGraph(): g = " + JSON.stringify(g));
+    var width = 960,
+        height = 500;
+
+    var color = d3.scale.category20();
+
+    var force = d3.layout.force()
+        .charge(-120)
+        .linkDistance(30)
+        .size([width, height]);
+
+    var svg = d3.select("#graph").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    force
+        .nodes(g.nodes)
+        .links(g.links)
+        .start();
+
+    var link = svg.selectAll("line.link")
+        .data(g.links)
+      .enter().append("line")
+        .attr("class", "link")
+        .style("stroke-width", function(d) { return Math.sqrt(d.weight); });
+
+    var node = svg.selectAll("circle.node")
+        .data(g.nodes)
+      .enter().append("circle")
+        .attr("class", "node")
+        .attr("r", 5) // TODO(choudhury): make the radius dependent on the node count.
+        .style("fill", function(d) { return color(d.type); })
+        .call(force.drag);
+
+    node.append("title")
+        .text(function(d) { return d.node; }); // TODO(choudhury): the field "node" should be renamed to "name".
+
+    force.on("tick", function(){
+        link.attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.source.x; })
+            .attr("y2", function(d) { return d.source.y; });
+
+        node.attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
+
+    });
 }
 
 function handleFileSelect(evt){
