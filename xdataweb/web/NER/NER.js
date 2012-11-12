e685a5c2e26f74eb25eb706a0877c6d6c92c163f
Roni Choudhury <roni.choudhury@kitware.com>
2012-11-12 12:14:15 -0500

    went back to old indentation scheme; new one is no good

--- xdataweb/web/NER/NER.js
+++ xdataweb/web/NER/NER.js
@@ -45,7 +45,7 @@ function processFile(filename, id){
         // Mark the appropriate list item as being processed.
         var li = d3.select("#" + id);
         li.html(NER.filenames[filename] + " processing")
-        .classed("processing inprogress", true);
+            .classed("processing inprogress", true);
 
         var file_hash = CryptoJS.MD5(text).toString();
         console.log("Checking hash for " + filename + "...");
@@ -113,8 +113,8 @@ function processFileContents(filename, id, file_hash){
 
         console.log("id = #" + id);
         var li = d3.select("#" + id)
-        .classed("inprogress", false)
-        .classed("processing done", true);
+            .classed("inprogress", false)
+            .classed("processing done", true);
         li.html(NER.filenames[filename] + ' processed');
 
         // If the "store" parameter is set to true, store the data in the
@@ -126,7 +126,7 @@ function processFileContents(filename, id, file_hash){
                 url: '/service/mongo/xdata/ner-cache',
                 data: {
                     file_hash: file_hash,
-                    data: data
+                data: data
                 }
             });
         }
@@ -138,59 +138,59 @@ function processFileContents(filename, id, file_hash){
             count: 1,
             index: NER.counter++
         };
-            var doc_index = NER.counter - 1;
+        var doc_index = NER.counter - 1;
 
-            // Extract the JSON object from the AJAX response.
-            var entities = $.parseJSON(data);
+        // Extract the JSON object from the AJAX response.
+        var entities = $.parseJSON(data);
 
-            // Process the entities.
-            $.each(entities, function(i, e){
-                // Place the entity into the global entity list
-                // if not already there.
-                //
-                // Also update the count of this entity.
-                var key = '["' + e[0] + '","' + e[1] + '"]';
-                if(!NER.nodes.hasOwnProperty(key)){
-                    NER.nodes[key] = {
-                        name: e[1],
-                        type: e[0],
-                        count: 1,
-                        index: NER.counter++
-                    };
-                }
-                else{
-                    NER.nodes[key].count++;
-                }
-                var entity_index = NER.nodes[key].index;
-
-                // Enter a link into the link list, or just increase the count if
-                // the link exists already.
-                var link = "(" + entity_index + "," + doc_index + ")";
-                if(!NER.links.hasOwnProperty(link)){
-                    NER.links[link] = {
-                        source: entity_index,
-                        target: doc_index,
-                        count: 1
-                    };
-                }
-                else{
-                    NER.links[link].count++;
-                }
-            });
-
-            //console.log(NER.nodes);
-
-            // Increment the number of successfully processed
-            // files; if the number reaches the number of total
-            // files to process, launch the final step of
-            // assembling the graph.
-            ++NER.files_processed;
-            console.log(NER.files_processed + " of " + NER.num_files + " processed");
-
-            if(NER.files_processed == NER.num_files){
-                graph.assemble(NER.nodes, NER.links);
-                graph.render();
+        // Process the entities.
+        $.each(entities, function(i, e){
+            // Place the entity into the global entity list
+            // if not already there.
+            //
+            // Also update the count of this entity.
+            var key = '["' + e[0] + '","' + e[1] + '"]';
+            if(!NER.nodes.hasOwnProperty(key)){
+                NER.nodes[key] = {
+                    name: e[1],
+            type: e[0],
+            count: 1,
+            index: NER.counter++
+                };
             }
+            else{
+                NER.nodes[key].count++;
+            }
+        var entity_index = NER.nodes[key].index;
+
+        // Enter a link into the link list, or just increase the count if
+        // the link exists already.
+        var link = "(" + entity_index + "," + doc_index + ")";
+        if(!NER.links.hasOwnProperty(link)){
+            NER.links[link] = {
+                source: entity_index,
+                target: doc_index,
+                count: 1
+            };
+        }
+        else{
+            NER.links[link].count++;
+        }
+        });
+
+        //console.log(NER.nodes);
+
+        // Increment the number of successfully processed
+        // files; if the number reaches the number of total
+        // files to process, launch the final step of
+        // assembling the graph.
+        ++NER.files_processed;
+        console.log(NER.files_processed + " of " + NER.num_files + " processed");
+
+        if(NER.files_processed == NER.num_files){
+            graph.assemble(NER.nodes, NER.links);
+            graph.render();
+        }
     };
 }
 
@@ -246,8 +246,8 @@ function handleFileSelect(evt){
         var li = d3.select("#file-info").append("li");
         NER.filenames[filename] = '<span class=filename>' + filename + '</span>';
         li.attr("id", id)
-        .classed("rejected", !using)
-        .html(NER.filenames[filename] + '<span class=filename>(' + (f.type || 'n/a') + ')</span> - ' + f.size + ' bytes ' + (using ? '<span class=ok>(ok)</span>' : '<span class=rejected>(rejected)</span>') );
+            .classed("rejected", !using)
+            .html(NER.filenames[filename] + '<span class=filename>(' + (f.type || 'n/a') + ')</span> - ' + f.size + ' bytes ' + (using ? '<span class=ok>(ok)</span>' : '<span class=rejected>(rejected)</span>') );
 
         if(using){
             var reader = new FileReader();
@@ -258,12 +258,12 @@ function handleFileSelect(evt){
 
     // Remove the "rejected" list items by fading them away.
     d3.selectAll("li.rejected")
-    .transition()
-    .delay(1000)
-    .duration(2000)
-    .style("opacity", 0.0)
-    .style("height", "0px")
-    .remove();
+        .transition()
+        .delay(1000)
+        .duration(2000)
+        .style("opacity", 0.0)
+        .style("height", "0px")
+        .remove();
 }
 
 window.onload = function(){
@@ -281,9 +281,9 @@ window.onload = function(){
             // times it occurs in the corpus.
             nodeScale: false,
 
-            // Whether to thicken a link proportionally to the number of times it
-            // occurs in the corpus.
-            linkScale: false 
+          // Whether to thicken a link proportionally to the number of times it
+          // occurs in the corpus.
+          linkScale: false 
         };
 
         var color = d3.scale.category20();
@@ -291,7 +291,7 @@ window.onload = function(){
         var svg = d3.select("#graph");
 
         var width = svg.attr("width"),
-        height = svg.attr("height");
+            height = svg.attr("height");
 
         return {
             assemble: function(nodedata, linkdata){
@@ -323,87 +323,87 @@ window.onload = function(){
                 });
             },
 
-            render: function(){
-                // Make sure the config is up-to-date.
-                this.updateConfig();
+                render: function(){
+                    // Make sure the config is up-to-date.
+                    this.updateConfig();
 
-                var force = d3.layout.force()
-                .charge(-120)
-                .linkDistance(30)
-                .size([width, height])
-                .nodes(nodes)
-                .links(links)
-                .start();
+                    var force = d3.layout.force()
+                        .charge(-120)
+                        .linkDistance(30)
+                        .size([width, height])
+                        .nodes(nodes)
+                        .links(links)
+                        .start();
 
-                var link = svg.selectAll("line.link")
-                .data(links)
-                .enter().append("line")
-                .classed("link", true)
-                .style("stroke-width", this.linkScalingFunction());
+                    var link = svg.selectAll("line.link")
+                        .data(links)
+                        .enter().append("line")
+                        .classed("link", true)
+                        .style("stroke-width", this.linkScalingFunction());
 
-                var node = svg.selectAll("circle.node")
-                .data(nodes)
-                .enter().append("circle")
-                .classed("node", true)
-                .attr("r", this.nodeScalingFunction())
-                .style("fill", function(d) { return color(d.type); })
-                .call(force.drag);
+                    var node = svg.selectAll("circle.node")
+                        .data(nodes)
+                        .enter().append("circle")
+                        .classed("node", true)
+                        .attr("r", this.nodeScalingFunction())
+                        .style("fill", function(d) { return color(d.type); })
+                        .call(force.drag);
 
-                node.append("title")
-                .text(function(d) { return d.name; });
+                    node.append("title")
+                        .text(function(d) { return d.name; });
 
-                force.on("tick", function(){
-                    link.attr("x1", function(d) { return d.source.x; })
-                    .attr("y1", function(d) { return d.source.y; })
-                    .attr("x2", function(d) { return d.target.x; })
-                    .attr("y2", function(d) { return d.target.y; });
+                    force.on("tick", function(){
+                        link.attr("x1", function(d) { return d.source.x; })
+                        .attr("y1", function(d) { return d.source.y; })
+                        .attr("x2", function(d) { return d.target.x; })
+                        .attr("y2", function(d) { return d.target.y; });
 
                     node.attr("cx", function(d) { return d.x; })
-                    .attr("cy", function(d) { return d.y; });
-                });
-            },
+                        .attr("cy", function(d) { return d.y; });
+                    });
+                },
 
-            updateConfig: function(){
-                // Sweep through the configuration elements and set the boolean
-                // flags appropriately.
-                var check = $("#nodefreq")[0];
-                config.nodeScale = check.checked;
+                updateConfig: function(){
+                    // Sweep through the configuration elements and set the boolean
+                    // flags appropriately.
+                    var check = $("#nodefreq")[0];
+                    config.nodeScale = check.checked;
 
-                check = $("#linkfreq")[0];
-                config.linkScale = check.checked;        
-            },
+                    check = $("#linkfreq")[0];
+                    config.linkScale = check.checked;        
+                },
 
-            applyConfig: function(){
-                // Reset the attributes on the nodes and links according to
-                // the current config settings.
-                svg.selectAll("line.link")
-                .transition()
-                .duration(2000)
-                .style("stroke-width", this.linkScalingFunction());
+                applyConfig: function(){
+                    // Reset the attributes on the nodes and links according to
+                    // the current config settings.
+                    svg.selectAll("line.link")
+                        .transition()
+                        .duration(2000)
+                        .style("stroke-width", this.linkScalingFunction());
 
-                svg.selectAll("circle.node")
-                .transition()
-                .duration(1000)
-                .attr("r", this.nodeScalingFunction());
-            },
+                    svg.selectAll("circle.node")
+                        .transition()
+                        .duration(1000)
+                        .attr("r", this.nodeScalingFunction());
+                },
 
-            nodeScalingFunction: function(){
-                if(config.nodeScale){
-                    return function(d) { return 5*Math.sqrt(d.count); };
+                nodeScalingFunction: function(){
+                    if(config.nodeScale){
+                        return function(d) { return 5*Math.sqrt(d.count); };
+                    }
+                    else{
+                        return 5;
+                    }
+                },
+
+                linkScalingFunction: function(){
+                    if(config.linkScale){
+                        return function(d) { return Math.sqrt(d.count); };
+                    }
+                    else{
+                        return 1;
+                    }
                 }
-                else{
-                    return 5;
-                }
-            },
-
-            linkScalingFunction: function(){
-                if(config.linkScale){
-                    return function(d) { return Math.sqrt(d.count); };
-                }
-                else{
-                    return 1;
-                }
-            }
         };
     })();
 
