721a718443ffe90e4d9b86410a4d0e3fc26b8374
Roni Choudhury <roni.choudhury@kitware.com>
2012-11-20 18:51:48 -0500

    getting closer to rendering actual bar chart

--- xdataweb/web/lib/barchart.js
+++ xdataweb/web/lib/barchart.js
@@ -6,12 +6,18 @@ var barchart = {};
 //barchart.barchart = function(table, xcolumn, ycolumn, svgselector, translate){
 barchart.barchart = function(options){
     // Extract options.
+    //
+    // TODO(choudhury): add parameters for background color/opacity and whether
+    // to place a border around the chart.
     var table = options.table;
     var xcolumn = options.xcolumn;
     var ycolumn = options.ycolumn;
     var svgselector = options.svgselector;
     var position = options.position;
     var size = options.size;
+    var margins = options.margins;
+    var border = null;
+    var background = null;
 
     // Raise exception for missing required options.
     required = ["table", "xcolumn", "ycolumn", "svgselector", "size"];
@@ -46,6 +52,21 @@ barchart.barchart = function(options){
         .style("stroke-opacity", 1.0)
         .attr("width", size[0])
         .attr("height", size[1]);
+
+    // Compute bar and gap widths.
+    //
+    // TODO(choudhury): make the proportion of bar width to be used for the gap
+    // into a parameter.
+    var barwidth = size[0] / table.length;
+    var gap = barwidth*0.20;
+    barwidth = barwidth*0.80;
+
+    // TODO(choudhury): Place axes (style properly).
+    //
+    // TODO(choudhury): Place bars (use y-column data, and x-column data for
+    // label).
+    //
+    // TODO(choudhury): Grow bars from 0 height and fade them in from 0 opacity.
 
     // TODO(choudhury): actual barchart content goes here.
     g.append("circle")
