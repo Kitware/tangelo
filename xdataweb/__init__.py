8103636bf92c5df3f25be29421b5f53538b25646
Roni Choudhury <roni.choudhury@kitware.com>
2012-11-30 10:19:21 -0500

    removed debug statement

--- xdataweb/__init__.py
+++ xdataweb/__init__.py
@@ -8,7 +8,6 @@ import xdataweb
 # with dynamic module loading.
 import os.path
 current_dir = os.path.dirname(os.path.abspath(__file__))
-print current_dir
 sys.path.append(current_dir)
 
 class Server(object):
