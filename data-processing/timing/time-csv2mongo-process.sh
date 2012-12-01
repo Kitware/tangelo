eac08c7d7d88d1ce11ddd20fc6b8fb8242388723
Roni Choudhury <roni.choudhury@kitware.com>
2012-12-01 18:05:43 -0500

    Adding shell scripts to carry out timing tests for MongoDB data upload.

---
+++ data-processing/timing/time-csv2mongo-process.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# This script provides a timing test of the csv2mongo.py script when it DOES
+# perform data conversion on the input CSV file.  The result can be compared
+# against the result of time-csv2mongo-bare.sh to see what kind of effect the
+# data conversion has on performance.
---
+++ data-processing/timing/time-mongoimport.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# This script provides a timing test of the mongoimport program when uploading a
+# dataset to a Mongo server.  This is a base timing for such uploads, as it
+# ships with MongoDB by default.
