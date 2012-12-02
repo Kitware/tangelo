1ff5ab1311627be64485f055b1199e8786004b60
Roni Choudhury <roni.choudhury@kitware.com>
2012-12-01 22:12:00 -0500

    added 'bundled' upload runs to timing test

--- data-processing/timing/time-all.sh
+++ data-processing/timing/time-all.sh
@@ -11,19 +11,37 @@ if [ -z "${file}" ]; then
 fi
 
 shift
-actions="$*"
+actions=$*
 
+# Perform timing runs.
 echo -n "Timing mongoimport..." >/dev/stderr
 mongoimport=`./time-mongoimport.sh ${file}`
 echo "done (${mongoimport} seconds)" >/dev/stderr
 
 echo -n "Timing csv2mongo (no processing)..." >/dev/stderr
-csv2mongobare=`./time-csv2mongo.sh ${file}`
+csv2mongobare=`./time-csv2mongo.sh ${file} 1`
 echo "done (${csv2mongobare} seconds)" >/dev/stderr
 
 echo -n "Timing csv2mongo (w/processing)..." >/dev/stderr
-csv2mongoprocess=`./time-csv2mongo.sh ${file} ${actions}`
+csv2mongoprocess=`./time-csv2mongo.sh ${file} 1 ${actions}`
 echo "done (${csv2mongoprocess} seconds)" >/dev/stderr
 
-echo "mongoimport,csv2mongo,csv2mongo (w/processing)"
-echo "${mongoimport},${csv2mongobare},${csv2mongoprocess}"
+echo -n "Timing csv2mongo (no processing, bundles of 10000)..." >/dev/stderr
+csv2mongobare10000=`./time-csv2mongo.sh ${file} 10000`
+echo "done (${csv2mongobare10000} seconds)" >/dev/stderr
+
+echo -n "Timing csv2mongo (w/processing, bundles of 10000)..." >/dev/stderr
+csv2mongoprocess10000=`./time-csv2mongo.sh ${file} 10000 ${actions}`
+echo "done (${csv2mongoprocess10000} seconds)" >/dev/stderr
+
+echo -n "Timing csv2mongo (no processing, bundles of 50000)..." >/dev/stderr
+csv2mongobare50000=`./time-csv2mongo.sh ${file} 50000`
+echo "done (${csv2mongobare50000} seconds)" >/dev/stderr
+
+echo -n "Timing csv2mongo (w/processing, bundles of 50000)..." >/dev/stderr
+csv2mongoprocess50000=`./time-csv2mongo.sh ${file} 50000 ${actions}`
+echo "done (${csv2mongoprocess50000} seconds)" >/dev/stderr
+
+# Report the results.
+echo "mongoimport,csv2mongo,csv2mongo (w/processing),csv2mongo (10000-bundles),csv2mongo (w/processing,10000-bundles),csv2mongo (50000-bundles),csv2mongo (w/processing,50000-bundles)"
+echo "${mongoimport},${csv2mongobare},${csv2mongoprocess},${csv2mongobare10000},${csv2mongoprocess10000},${csv2mongobare50000},${csv2mongoprocess50000}"
--- data-processing/timing/time-csv2mongo.sh
+++ data-processing/timing/time-csv2mongo.sh
@@ -10,8 +10,18 @@ if [ -z "${file}" ]; then
     exit 1
 fi
 
+bundle="$2"
+if [ -z "${bundle}" ]; then
+    echo "missing bundle parameter" >/dev/stderr
+    exit 1
+fi
+
+shift
+shift
+actions="$*"
+
 timer="/usr/bin/time -f %e"
-target="python2 ../csv2mongo.py --host mongo -d xdata -c upload-timing-test --drop -i ${file}"
+target="python2 ../csv2mongo.py --host mongo -d xdata -c upload-timing-test --drop -i ${file} -b ${bundle} ${actions}"
 
 # Perform the timing run.
 ${timer} -- ${target} 2>&1 >/dev/null
