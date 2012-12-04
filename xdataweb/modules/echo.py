03dc26576c66ae18443bda20463cb2421421dabe
Roni Choudhury <roni.choudhury@kitware.com>
2012-12-04 17:23:32 -0500

    Made echo service more general - it now handles positional args as well.

--- xdataweb/modules/echo.py
+++ xdataweb/modules/echo.py
@@ -2,12 +2,19 @@ import cherrypy
 
 # This service simply echoes back the URL-encoded arguments passed to it.
 class Handler:
-    def go(self, **kwargs):
+    def go(self, *pargs, **kwargs):
         cherrypy.response.headers['Content-type'] = 'text/plain'
         response = ""
+
+        # Dump the positional arguments.
+        if len(pargs) > 0:
+            response += "[" + ", ".join(pargs) + "]\n"
+
+        # Dump the keyword arguments.
         for k in kwargs:
             response += "%s -> %s\n" % (k, kwargs[k])
 
+        # Send the response back.
         if response != "":
             return response
         else:
