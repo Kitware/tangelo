5be525c72d7fa38f922ad06e70c91ac350a45c41
Roni Choudhury <roni.choudhury@kitware.com>
2012-12-05 16:42:28 -0500

    Added many more options for data processing.
    Restructured old-style "action" list so that arguments are more uniform and less
    scattered (e.g., date formats are specified directly within --date argument,
    rather than in parallel with subset of -a arguments).
    
    This commit moves towards solving issues #23 and #24; however, the script now
    runs much slower than the one in master that it was branched from (commit
    7606df1a38959c468b17df7fa97431937be04983).  Some Python profiling may be needed
    to figure out exactly what changed.

--- data-processing/csv2mongo.py
+++ data-processing/csv2mongo.py
@@ -12,15 +12,18 @@ if __name__ == '__main__':
     parser.add_argument("--host", required=True, help="the MongoDB server")
     parser.add_argument("-d", "--database", required=True, help="the database to use")
     parser.add_argument("-c", "--collection", required=True, help="the collection to use")
-    parser.add_argument("--drop", action='store_true', help="whether to drop the specified collection before beginning")
-    parser.add_argument("-a", "--action", action='append', nargs=2, help="a CSV field, and associated action to take ('float','int','date','clean-quotes'")
-    parser.add_argument("--date-format", action='append', help="date format string (supply once per 'date' action specified)")
-    parser.add_argument("-i", "--input", nargs='?', type=argparse.FileType('r'), default=sys.stdin, help="input file (defaults to stdin)")
-    parser.add_argument("-s", "--strict", action='store_true', help="Exits with error if any action fields do not exist in CSV header row")
-    parser.add_argument("-v", "--verbose", action='store_true', help="Print out records as they are processed")
-    parser.add_argument("-w", "--warning", action='store_true', help="Print out warnings when they occur")
-    parser.add_argument("-p", "--progress", type=int, default=0, help="Print out progress reports at intervals")
-    parser.add_argument("-b", "--bundle-size", type=int, default=1, help="Collect several records for uploading to server all together")
+    parser.add_argument("--drop-collection", action='store_true', help="drop the specified collection before beginning")
+    parser.add_argument("--convert", action='append', default=[], nargs=2, metavar=('FIELD', 'TYPE'), help="convert <FIELD> to <TYPE> before upload ('float' or 'int')")
+    parser.add_argument("--date", action='append', default=[], nargs=2, metavar=('FIELD', 'FMTSTRING'), help="convert <FIELD> to a date object, using <FMTSTRING>")
+    parser.add_argument("--location", nargs=3, metavar=('LATFIELD','LONGFIELD','NAME'), help="fuse <LATFIELD> and <LONGFIELD> fields into single field named <NAME> (<LATFIELD> and <LONGFIELD> will be converted to float)")
+    parser.add_argument("--hashtags", action='append', default=[], nargs=2, metavar=('FIELD','NAME'), help="extract hashtags from <FIELD> and put them in a new field named <NAME>")
+    parser.add_argument("--drop-field", action='append', default=[], metavar=('FIELD'), help="do not upload <FIELD> to database")
+    parser.add_argument("-i", "--input", nargs='?', metavar='FILE', type=argparse.FileType('r'), default=sys.stdin, help="input file (defaults to stdin)")
+    parser.add_argument("-s", "--strict", action='store_true', help="exits with error if any action fields do not exist in CSV header row")
+    parser.add_argument("-v", "--verbose", action='store_true', help="print out records as they are processed")
+    parser.add_argument("-w", "--warning", action='store_true', help="print out warnings when they occur")
+    parser.add_argument("-p", "--progress", type=int, default=0, metavar='NUM', help="print out progress reports every <NUM> records")
+    parser.add_argument("-b", "--bundle-size", type=int, default=1, metavar='NUM', help="upload <NUM> records at a time")
 
     # Use "vars" to get a dictionary of the parsed arguments.
     args = vars(parser.parse_args())
@@ -33,7 +36,12 @@ if __name__ == '__main__':
     host = args['host']
     database = args['database']
     collection = args['collection']
-    drop = args['drop']
+    drop_collection = args['drop_collection']
+    conversions = args['convert']
+    dates = args['date']
+    location = args['location']
+    hashtags = args['hashtags']
+    drop_fields = args['drop_field']
     infile = args['input']
     strict = args['strict']
     verbose = args['verbose']
@@ -41,33 +49,89 @@ if __name__ == '__main__':
     progress = args['progress']
     bundle_size = args['bundle_size']
 
-    # Construct a map directing how to process each field of the CSV file.
-    valid_actions = ['float', 'int', 'date']
-    i = 0
-    actions = {}
-    if args['action'] is not None:
-        for a in args['action']:
-            field = a[0]
-            action = a[1]
+    # Process the various requested data processing actions (convert, date,
+    # location, hashtags, and drop-field) into an action map.
+    #
+    # Type conversions.
+    valid_types = ['float', 'int']
+    convert = {}
+    for a in conversions:
+        # Extract the sub-arguments.
+        field = a[0]
+        convtype = a[1]
 
-            # Check that the requested action is valid.
-            if action not in valid_actions:
-                print >>sys.stderr, "%s: error: invalid action '%s'" % (progname, action)
-                sys.exit(1)
+        # Check that the requested conversion type is valid.
+        if convtype not in valid_types:
+            print >>sys.stderr, "%s: error: invalid conversion type '%s'" % (progname, convtype)
+            sys.exit(1)
 
-            # Install the action into the action table.
-            actions[field] = {'action' : action}
+        # Check that the field is not being repeated.
+        if field in convert:
+            print >>sys.stderr, "%s: error: field '%s' specified more than once for type conversion" % (progname, field)
+            sys.exit(1)
 
-            # If a field is specified as a date, grab the next date format string
-            # supplied by the user; bail out with an error if there are no more date
-            # format strings.
-            if action == 'date':
-                try:
-                    actions[field]['date-format'] = args['date_format'][i]
-                    i = i + 1
-                except IndexError:
-                    print >>sys.stderr, "%s: error: not enough date format strings" % (progname)
-                    sys.exit(1)
+        # Install the action in the table.
+        convert[field] = convtype
+
+    # Date conversions.
+    date = {}
+    for a in dates:
+        # Extract the sub-arguments.
+        field = a[0]
+        fmt = a[1]
+
+        # Make sure the field is not already in the 'convert' table.
+        if field in convert:
+            print >>sys.stderr, "%s: error: field '%s' appears for both type conversion and date conversion" % (progname, field)
+            sys.exit(1)
+
+        # Install the information in the table.
+        date[field] = fmt
+
+    # Location extraction.
+    if location is not None:
+        # Extract the sub-arguments.
+        latfield = args['location'][0]
+        longfield = args['location'][1]
+        name = args['location'][2]
+
+        # Record the information.
+        location = { 'latfield' : latfield,
+                     'longfield' : longfield,
+                     'name' : name }
+
+    # Hashtag extraction.
+    hashtag = {}
+    # Add the fields to the hashtag field set.
+    for a in hashtags:
+        # Extract the sub-arguments.
+        field = a[0]
+        name = a[1]
+
+        # Make sure field doesn't appear more than once.
+        if field in hashtag:
+            print >>sys.stderr, "%s: error: field '%s' appears more than once for hashtag extraction" % (progname, field)
+            sys.exit(1)
+
+        hashtag[field] = name
+
+    # Drop fields.
+    drop = set()
+    # Add the fields to the drop field set.
+    for a in drop_fields:
+        # Make sure the field doesn't appear in any data conversion
+        # operations.
+        if a in convert or a in date:
+            print >>sys.stderr, "%s: error: field '%s' appears both for type/date conversion and for dropping" % (progname, field)
+            sys.exit(1)
+
+        # Make sure the field doesn't appear more than once.
+        if a in drop:
+            print >>sys.stderr, "%s: error: field '%s' appears more than once for dropping" % (progname, field)
+            sys.exit(1)
+
+        # Place the field name in the drop table.
+        drop.add(a)
 
     # Create a connection to the Mongo database.
     try:
@@ -83,7 +147,7 @@ if __name__ == '__main__':
     db = conn[database]
 
     # Drop the collection before starting, if requested.
-    if drop:
+    if drop_collection:
         db.drop_collection(collection)
 
     # Get a handle to the collection.
@@ -98,9 +162,10 @@ if __name__ == '__main__':
     # Check that action fields all exist, if requested.
     if strict:
         missing = []
-        for f in actions.keys():
+        for f in convert.keys() + date.keys() + location.keys() + hashtag.keys() + list(drop):
             if f not in cols:
                 missing.append(f)
+
         if len(missing) > 0:
             print >>sys.stderr, "%s: error: the following action fields were missing from the data file: %s" % (progname,", ".join(missing))
             sys.exit(1)
@@ -114,52 +179,102 @@ if __name__ == '__main__':
         while len(row) < len(cols):
             row.append(None)
 
-        # Construct a dict to describe the row in terms of the headers.
+        # Construct a dict to describe the row in terms of the headers.  The
+        # "dropped" headers must be included in this step, in case they need to
+        # be processed into a location tuple or a hashtag list first.
         record = {}
         entries = zip(cols, row)
         for e in entries:
             record[e[0]] = e[1]
 
+        # Adjoin a location field if one exists in the command line arguments.
+        # The empty coordinates are to be filled in with lat/long values during
+        # processing below.
+        if location is not None:
+            record[location['name']] = [None, None]
+
         # Perform the requested operations on the appropriate columns.
-        for k in record:
+        for k in record.keys():
             # Test whether the data value is None, because if it is, it means it
             # was filled in by default to represent a missing value in the
             # original data.  In such a case, there is no need to process it.
-            if k in actions and record[k] != None:
-                action = actions[k]['action']
-                if action == 'float':
+            if record[k] is not None:
+                # Perform type conversion if requested.
+                if k in convert:
+                    convtype = convert[k]
+                    if convtype == 'float':
+                        try:
+                            record[k] = float(record[k])
+                        except ValueError:
+                            if strict or warning:
+                                print >>sys.stderr, "%s: warning: could not convert field '%s' to floating point value" % (progname, record[k])
+                            if strict:
+                                print >>sys.stderr, strict_mode_msg
+                                sys.exit(1)
+                    elif convtype == 'int':
+                        try:
+                            record[k] = int(record[k])
+                        except ValueError:
+                            if strict or warning:
+                                print >>sys.stderr, "%s: warning: could not convert field '%s' to integer value" % (progname, record[k])
+                            if strict:
+                                print >>sys.stderr, strict_mode_msg
+                                sys.exit(1)
+                    else:
+                        raise RuntimeError("invalid action '%s' encountered during processing" % (action))
+
+                # Perform date conversion if requested.
+                if k in date:
+                    fmt = date[k]
                     try:
-                        record[k] = float(record[k])
-                    except ValueError:
-                        if strict or warning:
-                            print >>sys.stderr, "%s: warning: could not convert field '%s' to floating point value" % (progname, record[k])
-                        if strict:
-                            print >>sys.stderr, strict_mode_msg
-                            sys.exit(1)
-                elif action == 'int':
-                    try:
-                        record[k] = int(record[k])
-                    except ValueError:
-                        if strict or warning:
-                            print >>sys.stderr, "%s: warning: could not convert field '%s' to floating point value" % (progname, record[k])
-                        if strict:
-                            print >>sys.stderr, strict_mode_msg
-                            sys.exit(1)
-                elif action == 'date':
-                    try:
-                        datefmt = actions[k]['date-format']
-                        record[k] = datetime.datetime.strptime(record[k], datefmt)
+                        record[k] = datetime.datetime.strptime(record[k], fmt)
                     except ValueError as e:
                         if strict or warning:
                             print >>sys.stderr, "%s: warning: could not convert field '%s' to a datetime object: %s" % (progname, record[k], e.message)
                         if strict:
                             print >>sys.stderr, strict_mode_msg
                             sys.exit(1)
-                else:
-                    raise RuntimeError("invalid action '%s' encountered during processing" % (action))
+
+                # Collect location data if requested.
+                #
+                # If both values are not supplied in the row, error checking
+                # below will catch it.
+                if location is not None:
+                    if k == location['latfield']:
+                        try:
+                            record[location['name']][0] = float(record[k])
+                        except ValueError:
+                            print >>sys.stderr, "%s: error: could not convert latitude field '%s' to a floating point value" % (progname, record[k])
+                            sys.exit(1)
+                    elif k == location['longfield']:
+                        try:
+                            record[location['name']][1] = float(record[k])
+                        except ValueError:
+                            print >>sys.stderr, "%s: error: could not convert longitude field '%s' to a floating point value" % (progname, record[k])
+                            sys.exit(1)
+
+                # Perform hashtag extraction if requested.
+                if k in hashtag:
+                    hashfield = hashtag[k]
+
+                    # Split apart the record field by whitespace, then filter by
+                    # first character being a hashmark.
+                    record[hashfield] = filter(lambda x: x[0] == '#', record[k].split())
+
+                # Now that the other processing is done, drop the field from the
+                # record if requested.
+                if k in drop:
+                    del record[k]
 
         if verbose:
             print record
+
+        # Make sure that location information is complete, if present.
+        if location is not None:
+            locdata = record[location['name']]
+            if locdata[0] is None or locdata[1] is None:
+                print >>sys.stderr, "%s: error: lat/long data in record is incomplete: %s" % (progname, record)
+                sys.exit(1)
 
         # Store the record in the current bundle.
         bundle.append(record)
