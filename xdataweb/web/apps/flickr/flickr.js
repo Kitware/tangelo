9866fd04786358535ea1e2e1a0542a8f66072bb2
Roni Choudhury <roni.choudhury@kitware.com>
2012-12-06 17:07:16 -0500

    Added a google map to the app.

--- xdataweb/web/apps/flickr/flickr.js
+++ xdataweb/web/apps/flickr/flickr.js
@@ -1,4 +1,9 @@
 window.onload = function(){
-    d3.select("#target")
-        .html("hello!");
+    var options = {
+        zoom: 3,
+        center: new google.maps.LatLng(65.67, 95.17),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var node = d3.select("#map").node();
+    var map = new google.maps.Map(node, options);
 }
