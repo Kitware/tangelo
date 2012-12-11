442df0aebadbdf644ec0546fe99c532d14cfe377
Jeffrey Baumes <jeff.baumes@kitware.com>
2012-12-11 10:39:12 -0500

    Adding data twiddle app for generating d3 visualizations

---
+++ xdataweb/modules/datatwiddle.py
@@ -0,0 +1,47 @@
+import pymongo
+import bson.json_util
+import json
+
+from xdataweb import empty_response
+
+class Handler:
+    def go(self, servername, dbname, collname, name=None, data=None, code=None):
+        # Construct an empty response object.
+        response = empty_response();
+
+        # If no schema was passed in, give an error.
+        #
+        # TODO(choudhury): see comment below about error codes, etc.
+        if name == None:
+            response['error'] = "no name"
+            return bson.json_util.dumps(response)
+
+        # Establish a connection to the MongoDB server.
+        try:
+            conn = pymongo.Connection(servername)
+        except pymongo.errors.AutoReconnect as e:
+            response['error'] = "error: %s" % (e.message)
+            return bson.json_util.dumps(response)
+
+        # Extract the requested database and collection.
+        db = conn[dbname]
+        coll = db[collname]
+
+        # If no data field was specified, treat this as a read request;
+        # otherwise, write the data to the database.
+        if data == None and code == None:
+            # Create a search schema for finding the record with the appropriate
+            # hash.
+            schema = {'_id' : name}
+
+            # Apply the schema to retrieve documents.
+            response['result'] = [d for d in coll.find(schema)]
+        else:
+            # Apply the schema to an insert request.
+            coll.save({'_id': name, 'data': data, 'code': code})
+
+            # Return a success code.
+            response['result'] = "ok"
+
+        # Convert to JSON and return the result.
+        return bson.json_util.dumps(response)
