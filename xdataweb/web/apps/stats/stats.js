08095e4eb4a0d08fe223b94ced0899619822b423
Roni Choudhury <roni.choudhury@kitware.com>
2012-12-20 15:01:38 -0500

    Added vgd3 support for compiling vis specs.

--- xdataweb/web/apps/stats/stats.js
+++ xdataweb/web/apps/stats/stats.js
@@ -1,11 +1,136 @@
+// Global namespace container.
 stats = {};
+
+// Some characteristics of the dataset under consideration.
 stats.start = null;
 stats.end = null;
 stats.count = null;
+
+// The JavaScript template to use for compiling the vis spec into.
 stats.vistemplate = null;
 
-function static_histogram(bins){
-    alert("static_histogram(" + bins + ")");
+// Data.
+stats.data = {};
+
+// A visualization spec template for generating a bar chart for the histogram
+// values.
+stats.spec = {
+    name: "histogram",
+    width: 800,
+    height: 400,
+    padding: {top: 30, bottom: 30, left: 35, right: 30},
+    data: [
+        {name: "values"}
+    ],
+    scales: [
+        {name: "x", type: "ordinal", domain: {data: "values", field: "bin"}, range: "width", padding: 0},
+        //{name: "y", domain: {data: "values", field: "value"}, range: "height"}
+        {name: "y", type: "linear", domain: [0,1], range: "height"}
+    ],
+    axes: [
+        {axis: "x", scale: "x", format: "%"},
+        {axis: "y", scale: "y"}
+    ],
+    marks: [
+        {
+            type: "rect",
+            name: "histogram_bars",
+            from: "values",
+            properties: {
+                x1: {scale: "x", field: "bin"},
+                y1: {scale: "y", field: "value"},
+                y2: {scale: "y", value: 0},
+                width: {scale: "x"},
+                fill: {value: "darkgreen"}
+            }
+        }
+    ]
+};
+
+function static_histogram(start, end, bins, sel, empty){
+    // Grab the chart container element.
+    var chart = d3.select(sel);
+
+    // Calculate the ranges of data.
+    var binsize = (end - start) / bins;
+    var ranges = [];
+    for(var i=0; i<bins+1; i++){
+        ranges.push(start + i*binsize);
+    }
+
+    // Create an empty array of histogram values.
+    stats.data.values = new Array(bins);
+
+    // Perform lookups to get counts of data for these regions.
+    //
+    // First, design a callback that must be called "bins" times before it does
+    // its actual work.
+    var make_chart = (function(){
+        var trigger = 1;
+
+        return function(){
+            // This mechanism causes the function's bulk not to execute until
+            // all of the AJAX calls have completed (i.e., until the function is
+            // called "bins" times, it will simply return early).
+            if(trigger != bins){
+                trigger += 1;
+                return;
+            }
+
+            // Empty the container if requested.
+            empty = empty || false;
+            if(empty){
+                chart.selectAll("*")
+                    .remove();
+            }
+
+            // Compile the spec into the template.
+            var source = vg.compile(stats.spec, stats.vistemplate);
+            d3.select("#code").text(source);
+            eval("stats.chart = " + source +";");
+            stats.vis = stats.chart();
+            stats.vis.el(sel).data(stats.data).init().update();
+        };
+    })();
+
+    // A callback function generator (this cannot be embedded in the loop below
+    // because of the weird way JavaScript binds the loop variable to its
+    // mention within the function body).
+    var callback_maker = function(which){
+        return function(response){
+            if(response.error !== null){
+                    console.log("error: could not complete database request for range " + which);
+                    console.log(response.error);
+                    return;
+            }
+
+            // Store the percentile range and number of records in the proper
+            // slot, and signal to the callback function that this AJAX call
+            // succeeded.
+            stats.data.values[which] = {
+                bin: (which + 1) / bins,
+                //value: response.result.count
+                value: +response.result.count / stats.count
+            };
+
+            make_chart();
+        };
+    }
+
+    // Now, fire several AJAX calls to retrieve the number of records existing
+    // in each range.
+    for(i=0; i<bins; i++){
+        $.ajax({
+            url: '/service/mongo/mongo/xdata/flickr_paris',
+            data: {
+                query: JSON.stringify({date: {$gte : {$date : ranges[i]}, $lt: {$date : ranges[i+1]}} }),
+                limit: "0",
+                fill: false
+            },
+            dataType: 'json',
+            success: callback_maker(i)
+        });
+    }
 }
 
 window.onload = function(){
@@ -30,7 +155,6 @@ window.onload = function(){
             // Capture the date by extracting the millisecond value and
             // converting it to a numeric type.
             stats.start = +response.result.data[0].date.$date;
-            console.log("start date: " + stats.start);
 
             // Trigger an AJAX call to get the *latest* date in the dataset.
             $.ajax({
@@ -50,7 +174,6 @@ window.onload = function(){
 
                     // Capture the date.
                     stats.end = +response.result.data[0].date.$date;
-                    console.log("end date: " + stats.end);
 
                     // Now fire an AJAX call to count the total number of
                     // records.
@@ -70,27 +193,25 @@ window.onload = function(){
 
                             // Save the count.
                             stats.count = +response.result.count;
-                            console.log("count: " + stats.count);
                             
                             // Fire an AJAX call to retrieve the Vega template
                             // text.
                             d3.text("/lib/vgd3-template.js.txt", function(text){
                                 // Grab the text.
                                 stats.vistemplate = text;
-                                console.log(stats.vistemplate);
 
                                 // Finally - now that we have all the stuff we
                                 // need - initialize the buttons.
                                 d3.select("#quartile").node().onclick = function(){
-                                    static_histogram(4);
+                                    static_histogram(stats.start, stats.end, 4, "#chart", true);
                                 };
 
                                 d3.select("#decile").node().onclick = function(){
-                                    static_histogram(10);
+                                    static_histogram(stats.start, stats.end, 10, "#chart", true);
                                 };
 
                                 d3.select("#percentile").node().onclick = function(){
-                                    static_histogram(100);
+                                    static_histogram(stats.start, stats.end, 100, "#chart", true);
                                 };
                             });
                         }
