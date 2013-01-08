847f1fa1c81aff2fbc322422b9633dfffc0fa6b5
Roni Choudhury <roni.choudhury@kitware.com>
2013-01-08 14:57:11 -0500

    Pink highlighting for current selection.

--- xdataweb/web/apps/stats/extra-update.template.js
+++ xdataweb/web/apps/stats/extra-update.template.js
@@ -1,30 +1,59 @@
-var titles = new Array(bins);
+var bardata = new Array(bins);
 var format = d3.format('%');
 var data = <<DATA>>;
 for(var i=0; i<bins; i++){
-    titles[i] = format(i/bins) + ' - ' + format((i+1)/bins) + ' (' + format(data[i].value) + ' or ' + data[i].count + ' records)';
+    bardata[i] = {};
+    bardata[i].title = format(i/bins) + ' - ' + format((i+1)/bins) + ' (' + format(data[i].value) + ' or ' + data[i].count + ' records)';
+    bardata[i].state = 'unselected';
 }
 var containers = dom.select('.mark-1').selectAll('rect');
-containers.data(titles).append('title').text(function(d) { return d; });
+containers.data(bardata).append('title').text(function(d) { return d.title; });
 
 containers.on('mouseover', function(d, i){
-    d3.select(this).style('opacity',0.3);
+    var select = function(d){
+        d.state = 'selected';
+        return d;
+    };
+
+    var unselect = function(d){
+        d.state = 'unselected';
+        return d;
+    };
+
+    var bar = d3.select(this);
+    if(d.state == 'selected'){
+        bar.style('fill', 'red')
+            .style('opacity', 0.5);
+    }
+    else if(d.state == 'unselected'){
+        bar.style('fill', 'black')
+            .style('opacity',0.3);
+    }
+    else{
+        console.log("d.state must be selected or unselected - was " + d.state);
+        throw 0;
+    }
+
     if(stats.dragging.on){
         var target = -1;
         if(i < stats.dragging.left){
             // Dragging outside the pack, to the left.
             for(var j=i; j<stats.dragging.left; j++){
                 var e = d3.select(containers[0][j]);
-                e.style('opacity', 0.3);
+                e.style('opacity', 0.3)
+                    .style('fill', 'red')
+                    .datum(select);
             }
             stats.dragging.left = i;
             stats.dragging.from = 0;
         }
         else if(i > stats.dragging.right){
             // Dragging outside the pack, to the right.
-            for(var j=stats.dragging.right+1; j<i; j++){
+            for(var j=stats.dragging.right+1; j<=i; j++){
                 var e = d3.select(containers[0][j]);
-                e.style('opacity', 0.3);
+                e.style('opacity', 0.3)
+                    .style('fill', 'red')
+                    .datum(select);
             }
             stats.dragging.right = i;
             stats.dragging.from = 1;
@@ -32,16 +61,22 @@ containers.on('mouseover', function(d, i){
         else{
             // Dragging inside the pack.
             if(stats.dragging.from === 0){
+                // Shrinking the selection from the left.
                 for(var j=stats.dragging.left; j<i; j++){
                     var e = d3.select(containers[0][j]);
-                    e.style('opacity', 0.0);
+                    e.style('opacity', 0.0)
+                        .style('fill', 'black')
+                        .datum(unselect);
                 }
                 stats.dragging.left = i;
             }
             else if(stats.dragging.from === 1){
+                // Shrinking the selection from the right.
                 for(var j=i+1; j<=stats.dragging.right; j++){
                     var e = d3.select(containers[0][j]);
-                    e.style('opacity', 0.0);
+                    e.style('opacity', 0.0)
+                        .style('fill', 'black')
+                        .datum(unselect);
                 }
                 stats.dragging.right = i;
             }
@@ -53,9 +88,14 @@ containers.on('mouseover', function(d, i){
     }
 });
 
-containers.on('mouseout', function(){
+containers.on('mouseout', function(d){
     if(!stats.dragging.on){
-        d3.select(this).style('opacity',0.0);
+        if(d.state === 'unselected'){
+            d3.select(this).style('opacity',0.0);
+        }
+        else if(d.state === 'selected'){
+            d3.select(this).style('opacity',0.3);
+        }
     }
 });
 
