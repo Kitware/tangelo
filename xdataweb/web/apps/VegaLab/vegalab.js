b0c442c7dd01ad2024eeeebdd673cc190e72e5a9
Roni Choudhury <roni.choudhury@kitware.com>
2013-01-11 14:06:12 -0500

    Basic web infrastructure up.

--- xdataweb/web/apps/VegaLab/vegalab.js
+++ xdataweb/web/apps/VegaLab/vegalab.js
@@ -1,5 +1,28 @@
+function textplop(textarea_id){
+    return function(e){
+        // Grab the filename from the element.
+        var file = d3.event.target.files[0];
+
+        // Instantiate a file reader object, whose callback will simply plop
+        // the text into the element.
+        var reader = FileReader();
+        reader.onload = function(e){
+            // Grab the text.
+            var text = e.target.result;
+
+            // Place it into the textarea element.
+            d3.select("#" + textarea_id)
+                .text(text);
+        };
+        reader.readAsText(file);
+    }
+}
+
 window.onload = function(){
-    d3.select(document.body)
-        .append('p')
-        .html("Hello");
+    // Install file upload handlers on the textarea buttons.
+    d3.select("#vega_file")
+        .on("change", textplop("vega"));
+
+    d3.select("#js_file")
+        .on("change", textplop("js"));
 };
