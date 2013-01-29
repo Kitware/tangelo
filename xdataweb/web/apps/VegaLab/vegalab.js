e3ec91e06876e493655e70a10216f4181b05b2e6
Roni Choudhury <roni.choudhury@kitware.com>
2013-01-28 19:20:09 -0500

    (Mostly) delinted vegalab.js (except for spurious (?) warning about same variable name appearing in different catch blocks).

--- xdataweb/web/apps/VegaLab/vegalab.js
+++ xdataweb/web/apps/VegaLab/vegalab.js
@@ -1,33 +1,44 @@
-vegalab = {};
+/*jslint evil: true, browser: true */
 
-function url_exists(url){
+/*global d3, vg, $, FileReader */
+
+var vegalab = {};
+
+function url_exists(url) {
+    "use strict";
+
     var http = new XMLHttpRequest();
     http.open('HEAD', url, false);
     http.send();
-    return http.status != 404;
+    return http.status !== 404;
 }
 
-function compile(){
+function compile() {
+    "use strict";
+
     // Capture the Vega and Javascript code.
-    var spectext = d3.select("#vega").node().value;
-    var jstext = d3.select("#js").node().value;
-    var datatext = d3.select("#data").node().value;
+    var spectext = d3.select("#vega").node().value,
+        jstext = d3.select("#js").node().value,
+        datatext = d3.select("#data").node().value,
+        spec,
+        source,
+        data,
+        d;
 
     // Create a JavaScript object out of the spec.
     //
     // TODO(choudhury): error checking for the eval call.
-    try{
+    try {
         spec = eval("(" + spectext + ")");
-    }
-    catch(e){
+    } catch (e) {
         console.log("error parsing Vega spec: " + e);
         return;
     }
 
     // Compile the Vega code into the template, and display it on the page.
-    var source = vg.compile(spec, vegalab.template);
+    source = vg.compile(spec, vegalab.template);
     d3.select("#code")
-       .text(source);
+        .text(source);
 
     // Eval the generated source, and capture the resulting function with a
     // name.
@@ -35,39 +46,35 @@ function compile(){
 
     // Eval the user Javascript, which should be just a function.  Default to
     // the function that does nothing.
-    if(jstext.length > 0){
-        try{
+    if (jstext.length > 0) {
+        try {
             eval("vegalab.js = " + jstext);
-        }
-        catch(e){
+        } catch (e) {
             console.log("error parsing user JavaScript: " + e);
             return;
         }
-    }
-    else{
-        vegalab.js = function() {};
+    } else {
+        vegalab.js = function () {};
     }
 
     // The data window should contain either a function that returns the data to
     // be used, or a JSON object describing the data directly.  Capture the
     // data, either way.
-    var data = null;
-    if(datatext.length > 0){
-        try{
+    data = null;
+    if (datatext.length > 0) {
+        try {
             // Try to parse a JSON object from the data window.
             data = $.parseJSON(datatext);
-        }
-        catch(e){
+        } catch (e) {
             // Reaching here means the data window could not be parsed as JSON.
             // Try to parse it as JavaScript now.  The window should contain a
             // single function that returns a data value.
-            try{
-                eval("var d = " + datatext);
+            try {
+                eval("d = " + datatext);
 
                 // Call the d() function, and store the result.
                 data = d();
-            }
-            catch(e){
+            } catch (e) {
                 console.log("error parsing user data: " + e);
                 return;
             }
@@ -80,7 +87,7 @@ function compile(){
         .selectAll("*")
         .remove();
     vegalab.chart = vegalab.make_chart();
-    vegalab.chart.load(function(){
+    vegalab.chart.load(function () {
         vegalab.chart.el("#chart").data(data).init().update();
     });
 
@@ -90,15 +97,20 @@ function compile(){
 
 // A callback function for reading a selected file and pasting its contents into
 // a textarea element.
-function textplop(textarea_id){
-    return function(e){
+function textplop(textarea_id) {
+    "use strict";
+
+    return function (e) {
+        var file,
+            reader;
+
         // Grab the filename from the element.
-        var file = d3.event.target.files[0];
+        file = d3.event.target.files[0];
 
         // Instantiate a file reader object, whose callback will simply plop
         // the text into the element.
-        var reader = new FileReader();
-        reader.onload = function(e){
+        reader = new FileReader();
+        reader.onload = function (e) {
             // Grab the text.
             var text = e.target.result;
 
@@ -106,39 +118,44 @@ function textplop(textarea_id){
             d3.select("#" + textarea_id).node().value = text;
         };
         reader.readAsText(file);
-    }
+    };
 }
 
-function loadspec(){
+function loadspec() {
+    "use strict";
+
+    var opt,
+        filename_prefix,
+        filename,
+        sel;
+
     // Check to see if it's the "dummy" option (which just states the name of
     // the pull down menu).
-    var sel = d3.select("#load").node();
-    if(sel.selectedIndex === 0){
+    sel = d3.select("#load").node();
+    if (sel.selectedIndex === 0) {
         return;
     }
 
     // Grab the name of the option's associated directory.
-    var opt = sel.options[sel.selectedIndex].id;
+    opt = sel.options[sel.selectedIndex].id;
 
     // Issue ajax calls to fill the textarea elements with the contents of the
     // appropriate files.
     //
     // NOTE: the directory name is the same as the filename base.
-    var filename_prefix = "/apps/VegaLab/examples/" + opt + "/" + opt;
+    filename_prefix = "/apps/VegaLab/examples/" + opt + "/" + opt;
 
     // Fill in the Vega spec text box.
-    var filename = filename_prefix + ".json";
-    if(url_exists(filename)){
-        d3.text(filename, function(err, text){
-            if(err !== null){
+    filename = filename_prefix + ".json";
+    if (url_exists(filename)) {
+        d3.text(filename, function (err, text) {
+            if (err !== null) {
                 console.log("error: could not read Vega spec '" + filename + "'");
-            }
-            else{
+            } else {
                 d3.select("#vega").node().value = text;
             }
         });
-    }
-    else{
+    } else {
         console.log("fatal error: Vega spec file '" + filename + "' does not exist");
 
         // Empty the textbox.
@@ -147,17 +164,15 @@ function loadspec(){
 
     // Fill in the JavaScript box.
     filename = filename_prefix + ".js";
-    if(url_exists(filename)){
-        d3.text(filename, function(err, text){
-            if(err !== null){
+    if (url_exists(filename)) {
+        d3.text(filename, function (err, text) {
+            if (err !== null) {
                 console.log("error: could not read JavaScript file '" + filename + "'");
-            }
-            else{
+            } else {
                 d3.select("#js").node().value = text;
             }
         });
-    }
-    else{
+    } else {
         console.log("info/warning: JavaScript file '" + filename + "' does not exist");
 
         // Empty the textbox.
@@ -167,29 +182,25 @@ function loadspec(){
     // Fill in the data box.
     //
     // Dispatch on the existence of a .js file or a .json file.
-    if(url_exists(filename_prefix + "-data.js")){
+    if (url_exists(filename_prefix + "-data.js")) {
         filename = filename_prefix + "-data.js";
-        d3.text(filename, function(err, text){
-            if(err !== null){
+        d3.text(filename, function (err, text) {
+            if (err !== null) {
                 console.log("error: could not read JavaScript file '" + filename + "'");
-            }
-            else{
+            } else {
                 d3.select("#data").node().value = text;
             }
         });
-    }
-    else if(url_exists(filename_prefix + "-data.json")){
+    } else if (url_exists(filename_prefix + "-data.json")) {
         filename = filename_prefix + "-data.json";
-        d3.text(filename, function(err, text){
-            if(err !== null){
+        d3.text(filename, function (err, text) {
+            if (err !== null) {
                 console.log("error: could not read JSON file '" + filename + "'");
-            }
-            else{
+            } else {
                 d3.select("#data").node().value = text;
             }
         });
-    }
-    else{
+    } else {
         console.log("info/warning: neither JavaScript file '" + (filename_prefix + "-data.js") + "' nor JSON file '" + (filename_prefix + "-data.json") + "' exists");
 
         // Empty the textbox.
@@ -197,10 +208,12 @@ function loadspec(){
     }
 }
 
-window.onload = function(){
+window.onload = function () {
+    "use strict";
+
     // Load the Vega template text.
-    d3.text("/lib/vgd3-template.js.txt", function(err, text){
-        if(err !== null){
+    d3.text("/lib/vgd3-template.js.txt", function (err, text) {
+        if (err !== null) {
             console.log("fatal error: could not read vega template file '/lib/vgd3-template.js.txt'");
             return;
         }
@@ -244,13 +257,13 @@ window.onload = function(){
     d3.select("#load")
         .on("change", loadspec)
         .selectAll("option")
-        .data(examples, function(d){ return d === undefined ? "" : d.optname; })
+        .data(examples, function (d) { return d === undefined ? "" : d.optname; })
         .enter()
         .append("option")
-        .attr("id", function(d){
+        .attr("id", function (d) {
             return d.dir;
         })
-        .html(function(d){
+        .html(function (d) {
             return d.optname;
         });
 };
