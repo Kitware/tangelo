87e9ea5a98f4c9712425c2ca3245ab9dbaa34322
Roni Choudhury <roni.choudhury@kitware.com>
2013-01-31 13:35:37 -0500

    Delinted id.js

--- xdataweb/web/lib/id.js
+++ xdataweb/web/lib/id.js
@@ -1,15 +1,21 @@
+/* jslint */
+
 // ID generator for different objects in use at the same time.
 
 // Global namespace.
 var ID = {};
 
-ID.next = (function(){
+ID.next = (function () {
+    "use strict";
+
     var counter = 0;
 
-    return function(){
-        var id = "id" + counter;
+    return function () {
+        var id;
+
+        id = "id" + counter;
         counter = counter + 1;
 
         return id;
-    }
-})();
+    };
+}());
