6a73c07c945c37041bb649c82c2b802f696ff63a
Roni Choudhury <roni.choudhury@kitware.com>
2013-01-31 14:51:22 -0500

    Delinted slider.js

--- xdataweb/web/lib/slider.js
+++ xdataweb/web/lib/slider.js
@@ -1,25 +1,35 @@
+/*jslint */
+
+/*globals $ */
+
 // Call this to create a single slider object within the element described by
 // sliderId.
-function slider(slider, callbacks){
+function slider(sliderelem, callbacks) {
+    "use strict";
+
+    var s,
+        onchange,
+        onslide,
+        config;
+
     // Make DOM element into a jQuery selction.
-    var slider = $(slider);
+    s = $(sliderelem);
 
     // Capture the user-supplied callbacks (missing ones will be captures as
     // "undefined").
-    var onchange = callbacks.onchange;
-    var onslide = callbacks.onslide;
+    onchange = callbacks.onchange;
+    onslide = callbacks.onslide;
 
     // Set up a config that uses the user-supplied callbacks.
-    var config = {
-        change: function(e, ui){
-            if(onchange){
+    config = {
+        change: function (e, ui) {
+            if (onchange) {
                 onchange(ui.value);
             }
         },
 
-        slide: function(e, ui){
-            //display.html(ui.value);
-            if(onslide){
+        slide: function (e, ui) {
+            if (onslide) {
                 onslide(ui.value);
             }
         }
@@ -27,35 +37,42 @@ function slider(slider, callbacks){
 
     // Return an interface object.
     return {
-        initialize: function() { slider.slider(config); },
-        setMin: function(min) { config.min = min; slider.slider(config); },
-        setMax: function(max) { config.max = max; slider.slider(config); },
-        getValue: function() { return slider.slider("value"); }
+        initialize: function () { s.slider(config); },
+        setMin: function (min) { config.min = min; s.slider(config); },
+        setMax: function (max) { config.max = max; s.slider(config); },
+        getValue: function () { return s.slider("value"); }
     };
 }
 
-function rangeSlider(slider, callbacks){
+function rangeSlider(sliderelem, callbacks) {
+    "use strict";
+
+    var slider,
+        onchange,
+        onslide,
+        config;
+
     // Make the slider element into a jQuery selection.
-    var slider = $(slider);
+    slider = $(sliderelem);
 
     // Capture the two callbacks (if these were not specified, they will be
     // captured as "undefined").
-    var onchange = callbacks.onchange;
-    var onslide = callbacks.onslide;
+    onchange = callbacks.onchange;
+    onslide = callbacks.onslide;
 
     // Set up a basic configuration that simply calls the user-supplied
     // callbacks.
-    var config = {
+    config = {
         range: true,
 
-        change: function(e, ui){
-            if(onchange){
+        change: function (e, ui) {
+            if (onchange) {
                 onchange(ui.values[0], ui.values[1]);
             }
         },
 
-        slide: function(e, ui){
-            if(onslide){
+        slide: function (e, ui) {
+            if (onslide) {
                 onslide(ui.values[0], ui.values[1]);
             }
         }
@@ -63,13 +80,13 @@ function rangeSlider(slider, callbacks){
 
     // Return the user an interface object.
     return {
-        initialize: function() { slider.slider(config); },
-        getMin: function() { return config.min; },
-        getMax: function() { return config.max; },
-        setMin: function(min) { config.min = min; slider.slider(config); },
-        setMax: function(max) { config.max = max; slider.slider(config); },
-        setLowValue: function(v) { slider.slider("values", [v, slider.slider("values")[1]]); },
-        setHighValue: function(v) { slider.slider("values", [slider.slider("values")[0], v]); },
-        getValue: function() { return slider.slider("values"); }
+        initialize: function () { slider.slider(config); },
+        getMin: function () { return config.min; },
+        getMax: function () { return config.max; },
+        setMin: function (min) { config.min = min; slider.slider(config); },
+        setMax: function (max) { config.max = max; slider.slider(config); },
+        setLowValue: function (v) { slider.slider("values", [v, slider.slider("values")[1]]); },
+        setHighValue: function (v) { slider.slider("values", [slider.slider("values")[0], v]); },
+        getValue: function () { return slider.slider("values"); }
     };
 }
