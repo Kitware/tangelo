ac2f55fb87575f50a6a3127665e6fac572ca490c
Roni Choudhury <roni.choudhury@kitware.com>
2013-02-05 18:41:12 -0500

    Properly named data file; fixed call to transition duration.

--- web/apps/VegaLab/examples/vega-arc/vega-arc.js
+++ web/apps/VegaLab/examples/vega-arc/vega-arc.js
@@ -1,10 +1,10 @@
 function (vis) {
     d3.selectAll(".mark-0 > *")
         .on("mouseover", function () {
-            d3.select(this).transition(1000).style("fill", "orange");
+            d3.select(this).transition().duration(250).style("fill", "orange");
         })
         .on("mouseout", function () {
-            d3.select(this).transition(1000).style("fill", "lightblue");
+            d3.select(this).transition().duration(250).style("fill", "lightblue");
         })
         .on("click", function (d) {
             alert("You clicked something size " + d.b);
