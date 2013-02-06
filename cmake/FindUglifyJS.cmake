db67fab5a41884444d339f07956c756459e27235
Roni Choudhury <roni.choudhury@kitware.com>
2013-02-06 17:44:21 -0500

    Fixing regression coming from merge.

--- cmake/FindUglifyJS.cmake
+++ cmake/FindUglifyJS.cmake
@@ -4,4 +4,4 @@ if(NOT UglifyJS_EXECUTABLE)
     find_program(UglifyJS_EXECUTABLE uglifyjs)
 endif()
 
-find_package_handle_standard_args(UglifyJS DEFAULT_MSG UGLIFYJS_EXECUTABLE)
+find_package_handle_standard_args(UglifyJS DEFAULT_MSG UglifyJS_EXECUTABLE)
