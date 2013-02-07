ef5dd32e81bbaa2472bb31aa53d043b5be3ae72f
Roni Choudhury <roni.choudhury@kitware.com>
2013-02-07 18:39:56 -0500

    Added documentation to remainder of extant library files.

--- web/lib/date.js
+++ web/lib/date.js
@@ -15,20 +15,55 @@
         day_names,
         toShortString;
 
+    /**
+     * @name date
+     * @memberOf xdw
+     *
+     * @namespace Some utilities for dealing with Date objects and dates in
+     * general.
+     */
     mod = xdw.namespace("date");
 
     month_names = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     day_names = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
+    /** Returns an array of abbreviated month names.
+     *
+     * @name monthNames
+     *
+     * @memberOf xdw.date
+     *
+     * @function
+     *
+     * @returns {string} The array of names.
+     */
     mod.monthNames = function () {
         return month_names.slice();
     };
 
+    /** Returns an array of abbreviated day names.
+     *
+     * @name dayNames
+     * @memberOf xdw.date
+     * @function
+     *
+     * @returns {Array} The array of names.
+     */
     mod.dayNames = function () {
         return day_names.slice();
     };
 
-    toShortString = function (d) {
+    /** Formats a date in the form "Oct 30, 1981 (05:31:00)"
+     *
+     * @name toShortString
+     * @memberOf xdw.date
+     * @function
+     *
+     * @param {Date} d The date to format.
+     *
+     * @return {string} The string representation of the date.
+     */
+    mod.toShortString = function (d) {
         var day,
             month,
             year,
