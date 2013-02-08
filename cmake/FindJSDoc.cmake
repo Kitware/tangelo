7e23e433ef8c2a96cbe9e984cc02a4243634b5f3
Jeffrey Baumes <jeff.baumes@kitware.com>
2013-02-08 12:11:50 -0500

    Adding more general-purpose FindJSDoc
    - Now requires using  (really just the java executable)
      followed by , then any custom parameters such as the js
      files you wish to process.
    
    - find_path could have paths added in the future for the default installation
      paths of jsdoc-toolkit in Linux distributions.

--- cmake/FindJSDoc.cmake
+++ cmake/FindJSDoc.cmake
@@ -1,7 +1,12 @@
 include(FindPackageHandleStandardArgs)
 
-if(NOT JSDoc_EXECUTABLE)
-    find_program(JSDoc_EXECUTABLE jsdoc)
-endif()
+find_package(Java REQUIRED)
 
-find_package_handle_standard_args(JSDoc DEFAULT_MSG JSDoc_EXECUTABLE)
+if(NOT JSDoc_PATH)
+    find_path(JSDoc_PATH jsrun.jar)
+endif()
+set(JSDoc_TEMPLATEPATH "${JSDoc_PATH}/templates/jsdoc")
+set(JSDoc_EXECUTABLE ${Java_JAVA_EXECUTABLE})
+set(JSDoc_ARGS -Djsdoc.dir=${JSDoc_PATH} -Djsdoc.template.dir=${JSDoc_TEMPLATEPATH} -jar ${JSDoc_PATH}/jsrun.jar ${JSDoc_PATH}/app/run.js)
+
+find_package_handle_standard_args(JSDoc DEFAULT_MSG JSDoc_PATH JSDoc_EXECUTABLE JSDoc_TEMPLATEPATH)
