34a74547b7b6d420d0dc27c1951f43e7db54399d
Roni Choudhury <roni.choudhury@kitware.com>
2013-03-09 19:03:24 -0500

    New JSON format for specifying both apps *and* external links.

--- web/index.js
+++ web/index.js
@@ -5,19 +5,30 @@
 window.onload = function () {
     "use strict";
 
-    d3.json("/apps.json", function (err, apps) {
+    d3.json("/apps.json", function (err, spec) {
+        var app,
+            apps,
+            col,
+            cols,
+            external,
+            i,
+            left,
+            right;
+
         if (err !== null) {
             console.log("fatal error: could not load app list from /apps.json");
             return;
         }
 
+        // Pull out the two lists in the specification - one for the list of
+        // apps, and one for the list of external links.
+        apps = spec.apps;
+        external = spec.external;
+
         // Grab a reference to each of the two index columns.
-        var left = d3.select("#left"),
-            right = d3.select("#right"),
-            cols = [left, right],
-            i,
-            col,
-            app;
+        left = d3.select("#left");
+        right = d3.select("#right");
+        cols = [left, right];
 
         // Place the app info/links into the two columns, alternating between
         // left and right.
@@ -32,5 +43,15 @@ window.onload = function () {
             col.append("p")
                 .html(app.description);
         }
+
+        // List out the external links in the unordered list.
+        d3.select("#external")
+            .data(external)
+            .append("li")
+            .html(function (d) {
+                return "<a href=\"" + d.link + "\">" + "<strong>" + d.name + "</strong>" + "</a>" +
+                    " (<a href=\"" + d.institution_link + "\">" + d.institution + "</a>) - " +
+                    d.description;
+            });
     });
 };
