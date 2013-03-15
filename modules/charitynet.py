e3aec8ec05ae0754e80104c628a5c971430f2148
Jeffrey Baumes <jeff.baumes@kitware.com>
2013-03-15 08:37:33 -0400

    Adding simple charitynet vega app and service.

---
+++ modules/charitynet.py
@@ -0,0 +1,33 @@
+import pymongo
+import json
+
+import xdataweb
+
+class Handler:
+    def go(self, servername, dbname, datatype):
+        # Construct an empty response object.
+        response = xdataweb.empty_response();
+
+        # Establish a connection to the MongoDB server.
+        try:
+            conn = pymongo.Connection(servername)
+        except pymongo.errors.AutoReconnect as e:
+            response['error'] = "error: %s" % (e.message)
+            return xdataweb.dumps(response)
+
+        # Extract the requested database and collection.
+        db = conn[dbname]
+
+        if datatype == "full":
+            # Output number of donors per county. Use a string prefixed by "0" if needed
+            # for the county code to output state codes less than 10 correctly.
+            coll = db["charitynet.normalized.donors.counties"]
+
+            # Do not use normal 'result' field so the URL be used directly in a vega
+            # specification.
+            response = [["%05d" % d['_id'], int(d['value'])] for d in coll.find() if d['_id'] != None]
+        else:
+            respose['error'] = "error: unknown datatype requested"
+
+        # Convert to JSON and return the result.
+        return xdataweb.dumps(response)
