d212253b260349b8ee6476fd2e8ad2d6a23ac244
Roni Choudhury <roni.choudhury@kitware.com>
2013-04-22 22:04:53 -0400

    Removing server.conf.in from project, and paring down tangelo.py to a true, runtime library for tangelo apps.

---
+++ tangelo.py
@@ -0,0 +1,22 @@
+import cherrypy
+
+# This function defines the structure of a service response.  Each service
+# module should import this function from this package.
+#
+# The payload is contained in the 'result' field, while the 'error' field can
+# indicate that something went wrong.  Posibly more fields could be added in the
+# future.
+def empty_response():
+    return {'result' : None,
+            'error' : None}
+
+def content_type(t=None):
+    r = cherrypy.response.headers['Content-type']
+
+    if t is not None:
+        cherrypy.response.headers['Content-type'] = t
+
+    return r
+
+def log(*pargs, **kwargs):
+    cherrypy.log(*pargs, **kwargs)
