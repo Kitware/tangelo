9a35eca427d881c9ee55053313559fdb12a73621
Patrick Gill <pgill@soteradefense.com>
2013-05-24 09:44:14 -0400

    removed unnecessary file

--- web/app/enron-titan/service/enron.groovy
+++
@@ -1,9 +0,0 @@
-def getEdgesForDate(vertexId, dateString, degrees) {
-    current = g.v(vertexId)
-    while(degrees > 0) {
-        current = current.bothE(dateString)
-        degrees--;
-    }
-    
-    return current
-}
