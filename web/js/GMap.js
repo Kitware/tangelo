740d79cc850b5b5a0c0e38b81c5a10f59ab8f689
Roni Choudhury <roni.choudhury@kitware.com>
2013-07-14 19:24:56 -0400

    Cleaned up GMap interface and fixed flickr app to follow suit.

--- web/js/GMap.js
+++ web/js/GMap.js
@@ -1,4 +1,6 @@
-/*global tangelo, d3, google */
+/*jslint browser: true */
+
+/*global tangelo, d3, google, $ */
 
 (function () {
     "use strict";
@@ -19,22 +21,15 @@
         // Record the container element.
         this.container = elem;
 
-        // Create an empty data array.
-        this.locationData = [];
-
-        // Store a null 'overlay' property, which will be filled in with a
-        // transparent SVG element when the overlay is sized and placed in the
-        // draw() callback.
-        this.overlay = null;
-
-        this.dayColor = d3.scale.category10();
-        this.monthColor = d3.scale.category20();
-
+        // Make the map live.
         this.setMap(this.map);
 
+        // Be sure to call the draw method on drag and resize events.
         that = this;
         google.maps.event.addListener(this.map, "drag", function () { that.draw(); });
+        $(window).resize(function () { that.draw(); });
 
+        // Fake an empty configuration if the user did not send one in.
         this.cfg = cfg || {};
     };
 
@@ -50,59 +45,32 @@
         // and add the SVG element to it.
         this.overlayLayer = this.getPanes().overlayMouseTarget;
 
-        var svg = this.svg = d3.select(this.overlayLayer).append("div")
-            .attr("id", "svgcontainer")
-            .style("position", "relative")
-            .style("left", "0px")
-            .style("top", "0px")
-            .append("svg");
-
-        // Add a debugging rectangle.
-        //svg.append("rect")
-        //.attr("id", "debugrect")
-        //.style("fill-opacity", 0.4)
-        //.style("fill", "white")
-        //.style("stroke", "black")
-        //.attr("width", svg.attr("width"))
-        //.attr("height", svg.attr("height"));
-
-        svg.append("g")
-            .attr("id", "markers");
-
-        // Record the SVG element in the object for later use.
-        this.overlay = svg.node();
-
-        // Add an SVG element to the map's div to serve as a color legend.
-        svg = d3.select(this.map.getDiv())
+        // Create an SVG element for the overlay, fixed to the top-left of the
+        // window (the size will be set later).
+        this.svg = d3.select(this.overlayLayer)
             .append("svg")
             .style("position", "fixed")
-            .style("top", "100px")
-            .style("right", "0px")
-            .attr("width", 100)
-            .attr("height", 570);
+            .style("top", "0px")
+            .style("left", "0px");
 
-        // Place a transparent rect in the SVG element to serve as its
-        // container.
-        //
-        /*        svg.append("rect")*/
-        //.attr("x", 0)
-        //.attr("y", 0)
-        //.attr("width", "100%")
-        //.attr("height", "100%")
-        //.style("stroke", "darkslategray")
-        //.style("fill-opacity", 0.1);
+        // Insert a top-level group element; the class will manage its translate
+        // property, and the user will make use of the element to manage what is
+        // rendered.
+        this.group = this.svg.append("g");
 
-        // Add an SVG group whose contents will change or disappear based on the
-        // active colormap.
-        //
-        //this.legend = svg.append("g").node();
+        // The translation offset for the group element.
+        this.groupOffset = {
+            x: 0,
+            y: 0
+        };
 
+        // Call the user's initialization function, passing in the top-level
+        // group element.
         if (this.cfg.initialize) {
-            this.cfg.initialize.call(this, svg.node());
+            this.cfg.initialize.call(this, this.group.node());
         }
     };
 
-    // draw() sizes and places the overlaid SVG element.
     tangelo.GMap.prototype.draw = function () {
         var proj,
             w,
@@ -126,37 +94,36 @@
             return;
         }
 
-        // Shift the container div to cover the "whole world".
-        //
-        // First, compute the pixel coordinates of the bounds of the "whole
-        // world".
+        // Compute the pixel offset with respect to the map of the SVG element.
         proj = this.getProjection();
         w = this.container.offsetWidth;
         h = this.container.offsetHeight;
         containerLatLng = proj.fromContainerPixelToLatLng({x: 0, y: 0});
         divPixels = proj.fromLatLngToDivPixel(containerLatLng);
 
-        // Move and resize the div element.
-        div = d3.select(this.overlayLayer).select("#svgcontainer");
-        newLeft = divPixels.x + "px";
-        newTop = divPixels.y + "px";
-        div.style("left", newLeft)
-            .style("top", newTop)
-            .style("width", w + "px")
-            .style("height", h + "px");
+        // TODO(choudhury): this is a big deal - we should NOT have to do
+        // completely different things to get the same behavior between Chrome
+        // and Firefox.  Figure out what's going on.
+        //
+        // Perform a translation of the SVG group element in order to move its
+        // contents along with the map.
+        if ($.browser.mozilla) {
+            this.groupOffset.x = -divPixels.x;
+            this.groupOffset.y = -divPixels.y;
+        } else if ($.browser.chrome) {
+            this.groupOffset.x = 0;
+            this.groupOffset.y = 0;
+        }
+        this.group.attr("transform", "translate(" + this.groupOffset.x + ", " + this.groupOffset.y + ")");
 
         // Resize the SVG element to fit the viewport.
-        svg = d3.select(this.overlayLayer).select("svg");
-        svg.attr("width", w)
+        this.svg.attr("width", w)
             .attr("height", h);
 
-        //// Make the rect element track the SVG element.
-        //svg.select("#debugrect")
-        //.attr("width", svg.attr("width"))
-        //.attr("height", svg.attr("height"));
-
+        // Call the user's draw function, passing in the group element and the
+        // projection.
         if (this.cfg.draw) {
-            this.cfg.draw.call(this, proj, svg.node(), divPixels);
+            this.cfg.draw.call(this, proj, this.group.node());
         }
     };
 
