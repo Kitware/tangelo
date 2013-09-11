a488221a217c76690710149ab2b19fd5af8b10f0
Jeffrey Baumes <jeff.baumes@kitware.com>
2013-09-11 13:35:20 -0400

    Disable bootstrap theme for now

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 # serve to show the default.
 
 import sys, os
-import sphinx_bootstrap_theme
+#import sphinx_bootstrap_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -94,15 +94,15 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'pyramid'
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+#html_theme = 'bootstrap'
+#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-	'bootswatch_theme': "readable"
-}
+#html_theme_options = {
+#	'bootswatch_theme': "readable"
+#}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
