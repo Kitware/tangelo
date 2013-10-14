2190c967459455202cea110f97c3c724da3dfd29
Roni Choudhury <roni.choudhury@kitware.com>
2013-10-14 18:30:12 -0400

    Replaced ad hoc image comparator with vtkTesting object.

--- testing/vtkweb/vtkweb-cone-test.py
+++ testing/vtkweb/vtkweb-cone-test.py
@@ -1,3 +1,4 @@
+import itertools
 import sys
 import time
 
@@ -27,7 +28,34 @@ if __name__ == "__main__":
         sys.exit(1)
 
     button.click()
-    time.sleep(3)
+
+    # Create signal classes to use for breaking out of the timeout loop below.
+    class Found: pass
+    class TimedOut: pass
+
+    # Set a timeout limit of 10 seconds; try again every half second.
+    timeout_limit = 10
+    delay = 0.5
+    try:
+        for i in itertools.count():
+            try:
+                # If there is an element with attribute "__vtkweb_viewport__" it
+                # means that the vtkweb process has started.
+                canvas = browser.find_element_by_css_selector("[__vtkweb_viewport__=true]")
+                raise Found()
+            except NoSuchElementException:
+                # If there is no such element, sleep for a short interval if we
+                # have not already timed out.
+                if delay * i > timeout_limit:
+                    raise TimedOut()
+                else:
+                    time.sleep(delay)
+    except TimedOut:
+        print >>sys.stderr, "fatal error: timed out while waiting for vtkweb process to start"
+        browser.quit()
+        sys.exit(1)
+    except Found:
+        pass
 
     # Grab the viewport element so we know where to put the mouse.
     div = browser.find_element_by_id("viewport")
