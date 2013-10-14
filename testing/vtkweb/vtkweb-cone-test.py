084a2e0386ac5c9734017684ac4eb2d8fa3d7424
Roni Choudhury <roni.choudhury@kitware.com>
2013-10-14 19:04:45 -0400

    Abstracted timeout function to utility library.  Changed phylotree test to use vtk.

--- testing/vtkweb/vtkweb-cone-test.py
+++ testing/vtkweb/vtkweb-cone-test.py
@@ -1,13 +1,10 @@
-import itertools
 import sys
 import time
 
-import selenium
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import NoSuchElementException
 
-from vtkwebtest import now, compare_images
+import vtkwebtest
 
 if __name__ == "__main__":
     # Create a Chrome window driver.
@@ -29,33 +26,9 @@ if __name__ == "__main__":
 
     button.click()
 
-    # Create signal classes to use for breaking out of the timeout loop below.
-    class Found: pass
-    class TimedOut: pass
-
-    # Set a timeout limit of 10 seconds; try again every half second.
-    timeout_limit = 10
-    delay = 0.5
-    try:
-        for i in itertools.count():
-            try:
-                # If there is an element with attribute "__vtkweb_viewport__" it
-                # means that the vtkweb process has started.
-                canvas = browser.find_element_by_css_selector("[__vtkweb_viewport__=true]")
-                raise Found()
-            except NoSuchElementException:
-                # If there is no such element, sleep for a short interval if we
-                # have not already timed out.
-                if delay * i > timeout_limit:
-                    raise TimedOut()
-                else:
-                    time.sleep(delay)
-    except TimedOut:
+    # Wait for the vtkweb process to start (but time out after 10 seconds).
+    if not vtkwebtest.wait_with_timeout(delay=0.5, limit=10, criterion=vtkwebtest.found_viewport(browser)):
         print >>sys.stderr, "fatal error: timed out while waiting for vtkweb process to start"
-        browser.quit()
-        sys.exit(1)
-    except Found:
-        pass
 
     # Grab the viewport element so we know where to put the mouse.
     div = browser.find_element_by_id("viewport")
@@ -72,11 +45,11 @@ if __name__ == "__main__":
     time.sleep(1)
 
     # Take a screenshot.
-    shot = "cone-%s.png" % (now())
+    shot = "cone-%s.png" % (vtkwebtest.now())
     browser.save_screenshot(shot)
 
     # Compare the screenshot with the baseline, and report to stdout.
-    print compare_images(shot, "baseline-cone.png")
+    vtkwebtest.compare_images(shot, "baseline-cone.png")
 
     # Close the browser window.
     browser.quit()
--- testing/vtkweb/vtkwebtest.py
+++ testing/vtkweb/vtkwebtest.py
@@ -1,7 +1,11 @@
 import datetime
 import itertools
+import time
+import types
 
 import vtk
+
+from selenium.common.exceptions import NoSuchElementException
 
 def now():
     return datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
@@ -18,3 +22,22 @@ def compare_images(test_img, baseline_img):
 
     # Perform the image comparison test and print out the result.
     t.RegressionTest(test_img, 0.0)
+
+def wait_with_timeout(delay=None, limit=None, criterion=None):
+    for i in itertools.count():
+        if criterion():
+            return True
+        elif delay * i > limit:
+            return False
+        else:
+            time.sleep(delay)
+
+def found_viewport(browser):
+    def func():
+        try:
+            browser.find_element_by_css_selector("[__vtkweb_viewport__=true]")
+            return True
+        except NoSuchElementException:
+            return False
+
+    return func
