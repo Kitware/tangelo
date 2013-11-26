f7f9cdb89d4329c6ae293952e9595a8ce8d36750
Roni Choudhury <roni.choudhury@kitware.com>
2013-11-26 10:47:20 -0500

    Converted geonodelink to a jquery plugin.

--- src/js/vis/geonodelink.js
+++ src/js/vis/geonodelink.js
@@ -1,41 +1,95 @@
 /*jslint browser: true, unparam: true */
 
-(function (tangelo, d3, vg) {
+(function ($, tangelo, vg) {
     "use strict";
 
-    tangelo.vis.geonodelink = function (spec) {
-        var nodeLatitude = tangelo.accessor(spec.nodeLatitude, 0),
-            nodeLongitude = tangelo.accessor(spec.nodeLongitude, 0),
-            nodeSize = tangelo.accessor(spec.nodeSize, 20),
-            nodeColor = tangelo.accessor(spec.nodeColor, 0),
-            linkColor = tangelo.accessor(spec.linkColor, 0),
-            linkSource = tangelo.accessor(spec.linkSource, 0),
-            linkTarget = tangelo.accessor(spec.linkTarget, 0),
-            vegaspec = spec.vegaspec,
-            data = spec.data,
-            that = {};
+    $.widget("tangelo.geonodelink", {
+        options: {
+            nodeLatitude: null,
+            nodeLongitude: null,
+            nodeSize: null,
+            nodeColor: null,
+            linkColor: null,
+            linkSource: null,
+            linkTarget: null,
+            data: null
+        },
 
-        data.nodes.forEach(function (d) {
-            d.latitude = nodeLatitude(d);
-            d.longitude = nodeLongitude(d);
-            d.size = nodeSize(d);
-            d.color = nodeColor(d);
-        });
-        data.links.forEach(function (d) {
-            d.color = linkColor(d);
-            d.source = linkSource(d);
-            d.target = linkTarget(d);
-        });
-        vg.parse.spec(vegaspec, function (chart) {
-            chart({el: spec.el, data: {table: data.nodes, links: data.links}}).update();
-        });
+        _missing: {
+            nodeLatitude: 0,
+            nodeLongitude: 0,
+            nodeSize: 20,
+            nodeColor: 0,
+            linkColor: 0,
+            linkSource: 0,
+            linkTarget: 0,
+            data: null
+        },
 
-        function update(spec) {
-            return that;
+        _create: function () {
+            var that = this,
+                vegaspec = this.options.vegaspec;
+
+            vg.parse.spec(vegaspec, function (chart) {
+                var options;
+
+                that.vis = chart;
+
+                options = $.extend(true, {}, that.options);
+                delete options.disabled;
+                delete options.create;
+                delete options.vegaspec;
+
+                that._setOptions(options);
+            });
+        },
+
+        _setOption: function (key, value) {
+            if (key !== "data") {
+                this._super(key, tangelo.accessor(value, this._missing[key]));
+            } else {
+                this._super(key, value);
+            }
+        },
+
+        _setOptions: function (options) {
+            var that = this,
+                doUpdate = false;
+
+            $.each(options, function (key, value) {
+                that._setOption(key, value);
+            });
+
+            this._update();
+        },
+
+        _update: function () {
+            var that = this;
+
+            $.each(this.options.data.nodes, function (i, v) {
+                var d = that.options.data.nodes[i];
+
+                d.latitude = that.options.nodeLatitude(d);
+                d.longitude = that.options.nodeLongitude(d);
+                d.size = that.options.nodeSize(d);
+                d.color = that.options.nodeColor(d);
+            });
+
+            $.each(this.options.data.links, function (i, v) {
+                var d = that.options.data.links[i];
+
+                d.color = that.options.linkColor(d);
+                d.source = that.options.linkSource(d);
+                d.target = that.options.linkTarget(d);
+            });
+
+            that.vis({
+                el: this.element.get(0),
+                data: {
+                    table: that.options.data.nodes,
+                    links: that.options.data.links
+                }
+            }).update();
         }
-
-        that.update = update;
-        return that;
-    };
-
-}(window.tangelo, window.d3, window.vg));
+    });
+}(window.jQuery, window.tangelo, window.vg));
