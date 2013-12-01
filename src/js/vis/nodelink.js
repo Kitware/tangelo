9e01789dbca9d14ce9f1b82e38e6f29ccf463732
Roni Choudhury <roni.choudhury@kitware.com>
2013-12-01 17:04:09 -0500

    Converted nodelink to a jquery plugin.

--- src/js/vis/nodelink.js
+++ src/js/vis/nodelink.js
@@ -1,84 +1,136 @@
 /*jslint browser: true */
 
-(function (tangelo, d3) {
+(function ($, tangelo, d3) {
     "use strict";
 
-    tangelo.vis.nodelink = function (spec) {
-        var colorScale,
-            sizeScale,
-            nodeColor = tangelo.accessor(spec.nodeColor, "steelblue"),
-            nodeSize = tangelo.accessor(spec.nodeSize, 10),
-            nodeLabel = tangelo.accessor(spec.nodeLabel, undefined),
-            linkSource = tangelo.accessor(spec.linkSource, 0),
-            linkTarget = tangelo.accessor(spec.linkTarget, 0),
-            width = spec.width || 1000,
-            height = spec.height || 1000,
-            force,
-            svg,
-            link,
-            node,
-            data = spec.data,
-            that = {};
+    $.widget("tangelo.nodelink", {
+        options: {
+            nodeColor: null,
+            nodeSize: null,
+            nodeLabel: null,
+            linkSource: null,
+            linkTarget: null,
+            width: 1000,
+            height: 1000,
+            data: null
+        },
 
-        data.links.forEach(function (d) {
-            d.source = linkSource(d);
-            d.target = linkTarget(d);
-        });
+        _missing: {
+            nodeColor: "steelblue",
+            nodeSize: 10,
+            nodeLabel: undefined,
+            linkSource: 0,
+            linkTarget: 0,
+            width: 1000,
+            height: 1000,
+        },
 
-        colorScale = d3.scale.category20();
-        sizeScale = d3.scale.sqrt()
-            .domain(d3.extent(data.nodes, function (d) { return nodeSize(d); }))
-            .range([5, 15]);
+        _create: function () {
+            var options;
 
-        force = d3.layout.force()
-            .charge(-120)
-            .linkDistance(30)
-            .size([width, height]);
+            this.colorScale = d3.scale.category20();
 
-        svg = d3.select(spec.el).append("svg")
-            .attr("width", width)
-            .attr("height", height);
+            this.options.width = this.options.width || 1000;
+            this.options.height = this.options.height || 1000;
 
-        force
-            .nodes(data.nodes)
-            .links(data.links)
-            .start();
+            this.force = d3.layout.force()
+                .charge(-120)
+                .linkDistance(30)
+                .size([this.options.width, this.options.height]);
 
-        link = svg.selectAll(".link")
-            .data(data.links);
-        link.enter().append("line")
-            .attr("class", "link")
-            .style("stroke", "black")
-            .style("stroke-width", 1);
+            this.svg = d3.select(this.element.get(0))
+                .append("svg")
+                .attr("width", this.options.width)
+                .attr("height", this.options.height);
 
-        node = svg.selectAll(".node")
-            .data(data.nodes)
-            .enter().append("circle")
-            .attr("class", "node")
-            .attr("r", function (d) { return sizeScale(nodeSize(d)); })
-            .style("fill", function (d) { return colorScale(nodeColor(d)); })
-            .call(force.drag);
+            options = $.extend(true, {}, this.options);
+            delete options.disabled;
+            delete options.create;
+            this._setOptions(options);
+        },
 
-        node.append("title")
-            .text(function(d) { return nodeLabel(d); });
+        _setOption: function (key, value) {
+            var that = this;
 
-        force.on("tick", function() {
-            link.attr("x1", function (d) { return d.source.x; })
-                .attr("y1", function (d) { return d.source.y; })
-                .attr("x2", function (d) { return d.target.x; })
-                .attr("y2", function (d) { return d.target.y; });
+            if (key !== "data" && key !== "width" && key !== "height") {
+                this._super(key, tangelo.accessor(value, this._missing[key]));
+            } else {
+                this._super(key, value);
+            }
+        },
 
-            node.attr("cx", function (d) { return d.x; })
-                .attr("cy", function (d) { return d.y; });
-        });
+        _setOptions: function (options) {
+            var that = this;
 
-        function update() {
-            return that;
+            $.each(options, function (key, value) {
+                that._setOption(key, value);
+            });
+
+            this._update();
+        },
+
+        _update: function () {
+            var that = this,
+                node,
+                link;
+
+            this.options.data.links.forEach(function (d) {
+                d.source = that.options.linkSource(d);
+                d.target = that.options.linkTarget(d);
+            });
+
+            this.sizeScale = d3.scale.sqrt()
+                .domain(d3.extent(this.options.data.nodes, function (d) {
+                    return that.options.nodeSize(d);
+                }))
+                .range([5, 15]);
+
+            this.force.size([this.options.width, this.options.height])
+                .nodes(this.options.data.nodes)
+                .links(this.options.data.links)
+                .start();
+
+            link = this.svg.selectAll(".link")
+                .data(this.options.data.links);
+
+            link.enter()
+                .append("line")
+                .classed("link", true)
+                .style("stroke", "black")
+                .style("stroke-width", 1);
+
+            node = this.svg.selectAll(".node")
+                .data(this.options.data.nodes);
+
+            node.enter()
+                .append("circle")
+                .classed("node", true)
+                .call(this.force.drag)
+                .append("title");
+
+            node.attr("r", function (d) {
+                    return that.sizeScale(that.options.nodeSize(d));
+                })
+                .style("fill", function (d) {
+                    return that.colorScale(that.options.nodeColor(d));
+                });
+
+            node.selectAll("title")
+                .text(function (d) {
+                    return that.options.nodeLabel(d);
+                });
+
+            this.force.on("tick", function () {
+                link.attr("x1", function (d) { return d.source.x; })
+                    .attr("y1", function (d) { return d.source.y; })
+                    .attr("x2", function (d) { return d.target.x; })
+                    .attr("y2", function (d) { return d.target.y; });
+
+                node.attr("cx", function (d) { return d.x; })
+                    .attr("cy", function (d) { return d.y; });
+            });
+
+            this.force.resume();
         }
-
-        that.update = update;
-
-        return that;
-    };
-
-}(window.tangelo, window.d3));
+    });
+}(window.jQuery, window.tangelo, window.d3));
