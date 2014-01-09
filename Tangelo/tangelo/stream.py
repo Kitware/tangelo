c1e05376dffd1e0f71109545af369afd4a306494
Roni Choudhury <roni.choudhury@kitware.com>
2014-01-09 16:42:28 -0500

    Better object standardization in streaming service.

--- Tangelo/tangelo/stream.py
+++ Tangelo/tangelo/stream.py
@@ -11,22 +11,21 @@ class TangeloStream(object):
 
     @cherrypy.expose
     def default(self, key=None):
+        # Construct a result object.
+        result = {}
+
         # Perform the requested action: GET means to either return a list of
         # stream keys, or to run the keyed stream one step; DELETE means to
         # delete a keyed stream from the registry.
         method = cherrypy.request.method
         if method == "GET":
             if key is None:
-                return json.dumps(self.streams.keys())
+                result = self.streams.keys()
             elif key not in self.streams:
-                return "\"Unknown key\""
+                result["error"] = "Unknown key"
             else:
                 # Grab the stream in preparation for running it.
                 stream = self.streams[key]
-
-                # Construct a result object.
-                result = {}
-                result["finished"] = False
 
                 # Attempt to run the stream via its next() method - if this yields a
                 # result, then continue; if the next() method raises StopIteration,
@@ -34,6 +33,7 @@ class TangeloStream(object):
                 # is raised, this is treated as an error.
                 try:
                     result["data"] = stream.next()
+                    result["finished"] = False
                 except StopIteration:
                     result["finished"] = True
                     del self.streams[key]
@@ -42,20 +42,21 @@ class TangeloStream(object):
                     raise cherrypy.HTTPError("501 Error in Python Service",
                             "Caught exception while executing stream service keyed by %s:<br><pre>%s</pre>" % (key, traceback.format_exc()))
 
-                try:
-                    return json.dumps(result)
-                except TypeError:
-                    raise cherrypy.HTTPError("501 Bad Response from Python Service",
-                            "The stream keyed by %s returned a non JSON-seriazable result: %s" % (key, result["data"]))
-
         elif method == "DELETE":
             if key is None:
-                return "\"No key specified\""
+                result["error"] = "No key specified"
             elif key not in self.streams:
-                return "\"Unknown key\""
+                result["error"] = "Unknown key"
             else:
                 del self.streams[key]
-                return "\"OK\""
+                result["data"] = "OK"
+
+        # JSON-serialize the result and return it.
+        try:
+            return json.dumps(result)
+        except TypeError:
+            raise cherrypy.HTTPError("501 Bad Response from Python Service",
+                    "The stream keyed by %s returned a non JSON-seriazable result: %s" % (key, result["data"]))
 
     def add(self, stream):
         # Generate a key corresponding to this object.
