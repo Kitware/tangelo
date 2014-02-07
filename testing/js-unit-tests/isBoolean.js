3e4f1448f0a441e9dcd5927bf46ea61a48b3ae1f
Roni Choudhury <roni.choudhury@kitware.com>
2014-02-07 17:57:43 -0500

    Adding blanket.js support, including testing infrastructure for tangelo.js coverage report.

---
+++ testing/js-unit-tests/isBoolean.js
@@ -0,0 +1,16 @@
+describe("isBoolean()", function () {
+    it("Test 1 - 'true' is a boolean", function () {
+        expect(tangelo.isBoolean(true))
+            .toBe(true);
+    });
+
+    it("Test 2 - 'false' is a boolean", function () {
+        expect(tangelo.isBoolean(false))
+            .toBe(true);
+    });
+
+    it("Test 2 - numbers are not boolean", function () {
+        expect(tangelo.isBoolean(450))
+            .toBe(false);
+    });
+});
