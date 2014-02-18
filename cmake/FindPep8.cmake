500a4499206ff348becd81d631c1840270401d5c
Roni Choudhury <roni.choudhury@kitware.com>
2014-02-18 17:58:13 -0500

    Removed unnecessary files.

--- cmake/FindPep8.cmake
+++
@@ -1,5 +0,0 @@
-if(NOT Pep8_EXECUTABLE)
-    find_program(Pep8_EXECUTABLE NAMES pep8 pep8-python2)
-endif()
-
-find_package_handle_standard_args(Pep8 DEFAULT_MSG Pep8_EXECUTABLE)
--- cmake/FindSphinx.cmake
+++
@@ -1,7 +0,0 @@
-include(FindPackageHandleStandardArgs)
-
-if(NOT Sphinx_EXECUTABLE)
-    find_program(Sphinx_EXECUTABLE sphinx-build sphinx-build2)
-endif()
-
-find_package_handle_standard_args(Sphinx DEFAULT_MSG Sphinx_EXECUTABLE)
