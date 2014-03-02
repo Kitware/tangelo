eed3d3bd3f441fc050dd7bc05bd0b78ef0653fe1
Roni Choudhury <roni.choudhury@kitware.com>
2014-03-02 14:02:54 -0500

    Revamped jasmine driver script

--- testing/phantomjs/run-jasmine.js
+++ testing/phantomjs/run-jasmine.js
@@ -1,53 +1,27 @@
 /*jslint browser: true */
 /*globals phantom */
 
-// This is a script for PhantomJS to run.  It drives Jasmine testing by
-// accepting a URL containing a Jasmine test suite, parsing that URL's contents
-// for appropriate test report data, and summarizing the report on the console.
+// This PhantomJS script drives Jasmine testing by accepting a URL containing a
+// Jasmine test suite, parsing that URL's contents for appropriate test report
+// data, and summarizing the report on the console.
 //
 // This is adapted from the "run-jasmine.js" script that comes with PhantomJS.
 
+// Parse command line arguments.
 var system = require('system');
-
-/** Wait until the test condition is true or a timeout occurs. Useful for
- * waiting on a server response or for a ui change (fadeIn, etc.) to occur.
- *
- * @param testFx javascript callback that evaluates to a boolean.
- *
- * @param onReady callback to invoke when testFx condition is fulfilled.
- *
- * @param timeOutMillis the max amount of time to wait. If not specified, 3 sec is used.
- */
-function waitFor(testFx, onReady, timeOutMillis) {
-    "use strict";
-
-    var maxtimeOutMillis = timeOutMillis || 3001, //< Default Max Timeout is 3s
-        start = new Date().getTime(),
-        condition = false,
-        interval = setInterval(function () {
-            if ((new Date().getTime() - start < maxtimeOutMillis) && !condition) {
-                // If not time-out yet and condition not yet fulfilled
-                condition = testFx();
-            } else {
-                if (!condition) {
-                    // If condition still not fulfilled (timeout but condition is 'false')
-                    console.log("'waitFor()' timeout");
-                    phantom.exit(1);
-                } else {
-                    // Condition fulfilled (timeout and/or condition is 'true')
-                    onReady(); //< Do what it's supposed to do once the condition is fulfilled
-                    clearInterval(interval); //< Stop this interval
-                }
-            }
-        }, 100); //< repeat check every 100ms
-}
-
 if (system.args.length !== 2) {
     console.log('Usage: run-jasmine.js URL');
     phantom.exit(1);
 }
 
+// Import PhantomJS webpage utilities.
 var page = require('webpage').create();
+
+// Extract the URL argument.
+var url = system.args[1];
+
+// Set a timeout (in milliseconds).
+var timeout = 3000;
 
 // Route "console.log()" calls from within the Page context to the main Phantom
 // context (i.e.  current "this")
@@ -57,51 +31,85 @@ page.onConsoleMessage = function (msg) {
     console.log(msg);
 };
 
-page.open(system.args[1], function (status) {
+// Open the URL.
+page.open(url, function (status) {
     "use strict";
 
     if (status !== "success") {
-        console.log("Unable to access network");
+        console.log("Could not retrieve URL (" + url + ")");
         phantom.exit(1);
     } else {
-        waitFor(function () {
-            return page.evaluate(function () {
-                return document.body.querySelector('.symbolSummary .pending') === null;
-            });
-        }, function () {
-            var i,
+        // Record the current time, for purposes of measuring the timeout.
+        var start = new Date().getTime();
+        setInterval(function () {
+            // This function, under setInterval, repeatedly checks the test page
+            // to see if any pending tests remain.  It will time out if it takes
+            // too long; otherwise, it parses the page to count how many tests
+            // have failed, and uses this number to indicate overall test
+            // passing/failure.
+            var now = new Date().getTime(),
+                done,
+                i,
                 exitCode;
 
-            exitCode = page.evaluate(function () {
-                var list = document.body.querySelectorAll('.results > #details > .specDetail.failed'),
-                    el,
-                    desc,
-                    msg,
-                    ret;
+            // If the test has timed out bail out with an error.
+            if (now - start > timeout) {
+                console.log("error: test timed out");
+                phantom.exit(1);
+            }
 
-                console.log(document.body.querySelector('.description').innerText);
-                if (list && list.length > 0) {
-                    console.log('');
-                    console.log(list.length + ' test(s) FAILED:');
-                    for (i = 0; i < list.length; i += 1) {
-                        el = list[i];
-                        desc = el.querySelector('.description');
-                        msg = el.querySelector('.resultMessage.fail');
+            // Examine the page to see if the tests are still running.
+            done = page.evaluate(function () {
+                return document.body.querySelector(".symbolSummary .pending") === null;
+            });
 
+            // If the tests are done, then count how many of them failed, print
+            // some errors if applicable, and exit with an appropriate error
+            // code.
+            if (done) {
+                exitCode = page.evaluate(function () {
+                    var list = document.body.querySelectorAll('.results > #details > .specDetail.failed'),
+                        el,
+                        desc,
+                        msg,
+                        ret;
+
+                    console.log(document.body.querySelector('.description').innerText);
+                    if (list && list.length > 0) {
                         console.log('');
-                        console.log(desc.innerText);
-                        console.log(msg.innerText);
-                        console.log('');
+                        console.log(list.length + ' test(s) FAILED:');
+                        for (i = 0; i < list.length; i += 1) {
+                            el = list[i];
+                            desc = el.querySelector('.description');
+                            msg = el.querySelector('.resultMessage.fail');
+
+                            console.log('');
+                            console.log(desc.innerText);
+                            console.log(msg.innerText);
+                            console.log('');
+                        }
+                        ret = 1;
+                    } else {
+                        console.log(document.body.querySelector('.alert > .passingAlert.bar').innerText);
+                        ret = 0;
                     }
-                    ret = 1;
-                } else {
-                    console.log(document.body.querySelector('.alert > .passingAlert.bar').innerText);
-                    ret = 0;
+
+                    return ret;
+                });
+
+                // If something goes wrong (e.g., a syntax or reference error in
+                // the JavaScript), print an error and set a non-zero exit code.
+                if (exitCode === null) {
+                    console.log("unexpected error: test did not run properly");
+                    exitCode = 1;
                 }
 
-                return ret;
-            });
-            phantom.exit(exitCode);
-        });
+                // Indicate test passing/failure to the runner.
+                phantom.exit(exitCode);
+            }
+        }, 100);
     }
+
+    // (Intentionally "fall off the end" here, so that setInterval() can run the
+    // page poller above.)
 });
