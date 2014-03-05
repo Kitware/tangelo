7a95875e65317137631b2f3d73cf5db6f1c73b22
Roni Choudhury <roni.choudhury@kitware.com>
2014-03-05 12:41:10 -0500

    Display stuff about the file when clicked.

--- tangelo/web/examples/girder-browser/index.js
+++ tangelo/web/examples/girder-browser/index.js
@@ -8,8 +8,27 @@ $(function () {
     $("#girder-browser").girderBrowser({
         label: "Girder",
         click: function (item, api) {
-            console.log(item);
-            window.location = [api, "item", item._id, "download"].join("/");
+            var div = d3.select("#file-info"),
+                link;
+
+            div.selectAll("*")
+                .remove();
+
+            div.append("p")
+                .html("<b>Name:</b> " + item.name);
+
+            div.append("p")
+                .html("<b>Created:</b> " + item.created);
+
+            div.append("p")
+                .html("<b>Updated:</b> " + item.updated);
+
+            div.append("p")
+                .html("<b>Size:</b> " + item.size);
+
+            link = [api, "item", item._id, "download"].join("/");
+            div.append("p")
+                .html("<a href=" + link + ">Download</a>");
         }
     });
 });
