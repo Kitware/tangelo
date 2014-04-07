b1bafd767897ad219f6ff35f4fcd1e1b3d6754e4
Roni Choudhury <roni.choudhury@kitware.com>
2014-04-07 11:56:51 -0400

    Created a basic view to handle the list of files.

--- tangelo/web/examples/lyra/index-backbone.js
+++ tangelo/web/examples/lyra/index-backbone.js
@@ -1,25 +1,36 @@
+// A model describing a file that contains a Lyra-edited visualization.
+LyraVis = Backbone.Model.extend({
+});
+
+// A collection describing all the Lyra vis files in a Girder instance.
+LyraVisList = Backbone.Collection.extend({
+});
+
+// A view that renders a LyraVis model.
+VegaView = Backbone.View.extend({
+});
+
+// A model describing a file that contains data for a Vega visualization.
+DataFile = Backbone.Model.extend({
+});
+
+// A collection describing all the data files in a Girder instance.
+DataFileList = Backbone.Collection.extend({
+});
+
+// A view that renders a list of files.
+FileListView = Backbone.View.extend({
+    tagName: "div",
+
+    render: function () {
+        var template = _.template($("#vis-files-view-template").html(), {});
+        this.$el.html(template);
+    }
+});
+
 $(function () {
-    // A model describing a file that contains a Lyra-edited visualization.
-    LyraVis = Backbone.Model.extend({
+    var visfiles = new FileListView({
+        el: "#vis-files-view"
     });
-
-    // A collection describing all the Lyra vis files in a Girder instance.
-    LyraVisList = Backbone.Collection.extend({
-    });
-
-    // A view that renders a LyraVis model.
-    VegaView = Backbone.View.extend({
-    });
-
-    // A model describing a file that contains data for a Vega visualization.
-    DataFile = Backbone.Model.extend({
-    });
-
-    // A collection describing all the data files in a Girder instance.
-    DataFileList = Backbone.Collection.extend({
-    });
-
-    // A view that renders a list of files.
-    FileListView = Backbone.View.extend({
-    });
+    visfiles.render();
 });
