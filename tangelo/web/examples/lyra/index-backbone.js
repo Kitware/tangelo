154b4239cc5ea36bc3a4f2683742ae6f80ddb53b
Roni Choudhury <roni.choudhury@kitware.com>
2014-04-07 11:10:41 -0400

    Adding a backbone version of the app.

---
+++ tangelo/web/examples/lyra/index-backbone.js
@@ -0,0 +1,25 @@
+$(function () {
+    // A model describing a file that contains a Lyra-edited visualization.
+    LyraVis = Backbone.Model.extend({
+    });
+
+    // A collection describing all the Lyra vis files in a Girder instance.
+    LyraVisList = Backbone.Collection.extend({
+    });
+
+    // A view that renders a LyraVis model.
+    VegaView = Backbone.View.extend({
+    });
+
+    // A model describing a file that contains data for a Vega visualization.
+    DataFile = Backbone.Model.extend({
+    });
+
+    // A collection describing all the data files in a Girder instance.
+    DataFileList = Backbone.Collection.extend({
+    });
+
+    // A view that renders a list of files.
+    FileListView = Backbone.View.extend({
+    });
+});
