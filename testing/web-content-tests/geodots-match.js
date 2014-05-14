3e11384db660fef895f5fe06f5f4529d7b470316
Roni Choudhury <roni.choudhury@kitware.com>
2014-05-14 14:45:32 -0400

    Added convenience function for common case of testing one element against baseline image

--- testing/web-content-tests/geodots-match.js
+++ testing/web-content-tests/geodots-match.js
@@ -10,28 +10,6 @@ declareTest({
     test: function (info) {
         "use strict";
 
-        var screencap,
-            diff,
-            diffMag;
-
-        // Get a screenshot of the canvas element.
-        screencap = new CanvasImage();
-        screencap.drawFromElement($("canvas").get(0));
-        screencap.savePNG("geodots-test.png");
-
-        // Compute a diff image against the baseline.
-        diff = diffImage(screencap, info.image.geodots);
-        diff.savePNG("geodots-diff.png");
-        diffMag = L2(diff);
-
-        // Print the L2 difference.
-        console.log("image difference (L2): " + diffMag);
-
-        cdashImage("TestImage", "geodots-test.png");
-        cdashImage("DifferenceImage", "geodots-diff.png");
-        cdashImage("ValidImage", info.imageFiles.geodots);
-
-        // Compare the diff magnitude to the tolerance.
-        return diffMag < 1e-8;
+        return compareImages(document.getElementsByTagName("canvas")[0], "geodots", "geodots", 1e-8);
     }
 });
