aac2af2045b94e67127de092787d07160af36bf3
Jeffrey Baumes <jeff.baumes@kitware.com>
2014-05-21 14:25:47 -0400

    Fully testing tangelo.accessor, fixes #332

--- testing/test-cases/js-unit-tests/accessor.js
+++ testing/test-cases/js-unit-tests/accessor.js
@@ -4,60 +4,58 @@ describe("tangelo.accessor", function () {
     "use strict";
 
     var data = {
-        oranges: "tangelos",
-        lemons: {
-            car: "jalopy",
-            fruit: "citrus"
-        }
-    },
-    undef1 = tangelo.accessor(),
-    undef2 = tangelo.accessor({}),
-    func = tangelo.accessor(function (x, y) {
-        return x + y;
-    }),
-    value = tangelo.accessor({value: 10}),
-    index = tangelo.accessor({index: true}),
-    field1 = tangelo.accessor({field: "oranges"}),
-    field2 = tangelo.accessor({field: "lemons.car"});
+            oranges: "tangelos",
+            lemons: {
+                car: "jalopy",
+                fruit: "citrus"
+            }
+        };
 
     it("Undefined accessors display 'undefined' property", function () {
+        var undef1 = tangelo.accessor(),
+            undef2 = tangelo.accessor({});
         expect(undef1.undefined).toBe(true);
         expect(undef2.undefined).toBe(true);
     });
 
     it("Undefined accessor throws exception when called", function () {
-        expect(undef1).toThrow();
-        expect(undef2).toThrow();
+        expect(tangelo.accessor()).toThrow();
+        expect(tangelo.accessor({})).toThrow();
     });
 
     it("Defined accessors do not display 'undefined' property", function () {
-        expect(func.undefined).toBe(undefined);
+        var value = tangelo.accessor({value: 10});
         expect(value.undefined).toBe(undefined);
-        expect(index.undefined).toBe(undefined);
-        expect(field1.undefined).toBe(undefined);
-        expect(field2.undefined).toBe(undefined);
-    });
-
-    it("Regular functions are accessors", function () {
-        expect(func(3, 4)).toBe(7);
     });
 
     it("Value spec produces accessor", function () {
+        var value = tangelo.accessor({value: 10});
         expect(value(data)).toBe(10);
     });
 
     it("Index spec produces accessor", function () {
+        var index = tangelo.accessor({index: true});
         expect(index(data, 5)).toBe(5);
     });
 
     it("Field spec produces accessor", function () {
+        var field1 = tangelo.accessor({field: "oranges"}),
+            field2 = tangelo.accessor({field: "lemons.car"}),
+            field3 = tangelo.accessor({field: "."});
         expect(field1(data)).toBe("tangelos");
         expect(field2(data)).toBe("jalopy");
+        expect(field2({})).toBe(undefined);
+        expect(field3(4)).toBe(4);
     });
 
     it("Unknown spec throws exception", function () {
         expect(function () {
             return tangelo.accessor({invalid: "quux"});
         }).toThrow();
+        expect(tangelo.accessor(undefined)).toThrow();
+    });
+
+    it("Clone a function (twice)", function () {
+        expect(tangelo.accessor(tangelo.accessor(function (d) { return d; }))(10)).toBe(10);
     });
 });
