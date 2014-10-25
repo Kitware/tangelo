2065ea2aedb1beacacdf87c0028cc4c84ae0cae6
Roni Choudhury <roni.choudhury@kitware.com>
2014-10-25 16:32:11 -0400

    Added "configuration" task to find virtualenv binary.
    Removed jshint/nodeunit tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,24 @@
 /*global module:false*/
+var fs = require("fs");
+
 module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
     // Task configuration.
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        globals: {}
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
-      }
-    },
-    nodeunit: {
-      files: ['test/**/*_test.js']
+    prompt: {
+        virtualenv: {
+            options: {
+                questions: [
+                    {
+                        config: "virtualenv",
+                        type: "input",
+                        message: "Path to virtualenv?",
+                        default: "/usr/bin/virtualenv"
+                    }
+                ]
+            }
+        }
     },
     watch: {
       gruntfile: {
@@ -42,9 +33,33 @@ module.exports = function(grunt) {
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks("grunt-prompt");
   grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Configuration task.
+  grunt.registerTask("configure", "Write configuration options to disk", function () {
+    var text;
+
+    // If there is no config file already, then schedule the approprriate prompt
+    // task, followed by a retry of the current task, then bail.
+    if (!grunt.config("virtualenv")) {
+        grunt.task.run("prompt:virtualenv");
+        grunt.task.run("configure");
+        return;
+    }
+
+    // Build a configuration object from the config values.
+    text = JSON.stringify({
+        virtualenv: grunt.config("virtualenv")
+    }, null, 4) + "\n";
+
+    // Serialize to disk.
+    try {
+        fs.writeFileSync("configuration.json", text);
+    } catch (e) {
+        grunt.fail.warn("Could not write configuration.json" + e);
+    }
+  });
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'nodeunit']);
