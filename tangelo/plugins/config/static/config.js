29ee28512187189156d7629875b58526fec9afd5
Roni Choudhury <roni.choudhury@kitware.com>
2014-11-12 10:52:52 -0500

    Config plugin with error handling.

--- tangelo/plugins/config/static/config.js
+++ tangelo/plugins/config/static/config.js
@@ -1,57 +1,55 @@
-// Returns a key-value store containing the configuration options encoded in the
-// inputfile.
-tangelo.getPlugin("config").config = function (cfg) {
+// Attempts to retrieve a JSON-encoded configuration from `url`.  On success,
+// the JSON object is passed to `callback`.  If there is not a file at `url`,
+// and `required` is set to false, `callback` is invoked with an empty object.
+// An error results in `callback` being invoked with undefined as the first
+// argument and the error string as the second.  The `required` parameter can be
+// omitted, and defaults to false in that case.
+tangelo.getPlugin("config").config = function (url, required, callback) {
     var url,
         required,
         callback,
         inputfile;
 
-    // Get configuration parameters.
-    cfg = cfg || {};
-    url = cfg.url;
-    required = cfg.required || false;
-    callback = cfg.callback || $.noop;
-
     if (url === undefined) {
         throw new Error("'url' parameter is required");
     }
 
-    inputfile = tangelo.absoluteUrl(url);
-/*    var path = window.location.pathname;*/
+    // This allows a default value for the `required` parameter.
+    if (callback === undefined && tangelo.isFunction(required)) {
+        callback = required;
+        required = false;
+    }
 
-    //// If the current location ends in a filename, compute the
-    //// containing directory before appending the input file name.
-    //if (path.slice(-1) !== "/") {
-        //path = window.location.pathname.split("/").slice(0, -1).join("/");
-    //}
+    // Convert the URL to an absolute URL.
+    url = tangelo.absoluteUrl(url);
 
-    //if (inputfile.length > 0) {
-        //if (inputfile[0] !== "/" && inputfile[0] !== "~") {
-            //inputfile = path + "/" + inputfile;
-        //}
-    //}
-
+    // Fire the request to the config service.
     $.ajax({
-        url: "/service/config",
-        data: {
-            path: inputfile
-        },
+        url: tangelo.pluginUrl("config", "config", url),
         dataType: "json",
         error: function (jqxhr) {
-            // If the ajax call fails, pass the request object to the
-            // function so the client can examine it.
-            callback(undefined, undefined, jqxhr);
+            switch (jqxhr.statusCode()) {
+                case 400:
+                    callback(undefined, jqxhr.response.error, jqxhr);
+                    break;
+
+                case 404:
+                    if (required) {
+                        callback(undefined, jqxhr.response.error, jqxhr);
+                    } else {
+                        // Pass in an empty config if the user did not require a
+                        // configuration file to be found.
+                        callback({});
+                    }
+                    break;
+
+                default:
+                    callback(undefined, undefined, jqxhr);
+                    break;
+            }
         },
         success: function (data) {
-            // If successful, check for errors in the execution of the
-            // service itself, passing that error to the callback if
-            // necessary.  Otherwise, pass the status and data along to the
-            // callback.
-            if (data.error) {
-                callback(undefined, data.error, undefined);
-            } else {
-                callback(data.result);
-            }
+            callback(data.result);
         }
     });
 };
