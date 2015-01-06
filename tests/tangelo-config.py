673090487269751e533945aed8424a90f8ecfb97
Roni Choudhury <roni.choudhury@kitware.com>
2015-01-06 16:37:20 -0500

    Refactoring bad config test

--- tests/tangelo-config.py
+++ tests/tangelo-config.py
@@ -4,8 +4,10 @@ import fixture
 def test_bad_config():
     (code, stdout, stderr) = fixture.run_tangelo("-c", "tests/config/bad-config.yaml")
 
-    print "Expected: 'ERROR while parsing' in log"
-    print "Received: %s" % (stdout[1] if len(stdout) > 1 else "(nothing)")
+    signal = "ERROR while parsing"
+
+    print "Expected: '%s' in second line of log" % (signal)
+    print "Received: %s" % (stdout[1] if len(stdout) > 1 else "")
 
     assert len(stdout) > 1
-    assert "ERROR while parsing" in stdout[1]
+    assert signal in stdout[1]
