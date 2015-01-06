38fc4adce89d3be07d005121f50594adb8279ad6
Roni Choudhury <roni.choudhury@kitware.com>
2015-01-06 16:40:13 -0500

    Adding test for non-dict config file

--- tests/tangelo-config.py
+++ tests/tangelo-config.py
@@ -11,3 +11,15 @@ def test_bad_config():
 
     assert len(stdout) > 1
     assert signal in stdout[1]
+
+
+def test_non_dict_config():
+    (code, stdout, stderr) = fixture.run_tangelo("-c", "tests/config/list-config.yaml")
+
+    signal = "does not contain associative array"
+
+    print "Expected: '%s' in second line of log" % (signal)
+    print "Received: %s" % (stdout[1] if len(stdout) > 1 else "")
+
+    assert len(stdout) > 1
+    assert signal in stdout[1]
