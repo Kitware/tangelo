0381fe32664e246011d5917a81c81fce936ae364
David Manthey <david.manthey@kitware.com>
2015-10-01 13:52:25 -0400

    Reorder when the tangelo instance gets shut down in a test so that if an assert fails, other tests will still be able to run.

--- tests/tangelo-verbose.py
+++ tests/tangelo-verbose.py
@@ -5,15 +5,15 @@ def test_standard_verbosity():
     stderr = fixture.start_tangelo(stderr=True)
     stderr = '\n'.join(stderr)
 
+    fixture.stop_tangelo()
     assert 'TANGELO Server is running' in stderr
     assert 'TANGELO Hostname' in stderr
-    fixture.stop_tangelo()
 
 
 def test_lower_verbosity():
     stderr = fixture.start_tangelo("-q", stderr=True)
     stderr = '\n'.join(stderr)
 
+    fixture.stop_tangelo()
     assert 'TANGELO Server is running' in stderr
     assert 'TANGELO Hostname' not in stderr
-    fixture.stop_tangelo()
