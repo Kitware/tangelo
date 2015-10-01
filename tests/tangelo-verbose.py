7625a0bedc61229070ac6f246f026a1a48092a82
David Manthey <david.manthey@kitware.com>
2015-10-01 12:10:32 -0400

    Remove some debug statements.

--- tests/tangelo-verbose.py
+++ tests/tangelo-verbose.py
@@ -5,8 +5,6 @@ def test_standard_verbosity():
     stderr = fixture.start_tangelo(stderr=True)
     stderr = '\n'.join(stderr)
 
-    print stderr
-
     assert 'TANGELO Server is running' in stderr
     assert 'TANGELO Hostname' in stderr
     fixture.stop_tangelo()
@@ -15,8 +13,6 @@ def test_standard_verbosity():
 def test_lower_verbosity():
     stderr = fixture.start_tangelo("-q", stderr=True)
     stderr = '\n'.join(stderr)
-
-    print stderr
 
     assert 'TANGELO Server is running' in stderr
     assert 'TANGELO Hostname' not in stderr
