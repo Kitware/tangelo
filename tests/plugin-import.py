62d73dd8785e65be3cb1d0f4244937d95255ba5c
Roni Choudhury <roni.choudhury@kitware.com>
2015-10-07 11:43:47 -0400

    Changing single quotes to double quotes

--- tests/plugin-import.py
+++ tests/plugin-import.py
@@ -3,50 +3,50 @@ import json
 
 
 def test_plugin_module():
-    config = {'plugins': [{
-        'name': 'moduletest',
-        'path': 'tests/plugins/moduletest'
+    config = {"plugins": [{
+        "name": "moduletest",
+        "path": "tests/plugins/moduletest"
     }]}
-    (_, _, stderr) = fixture.run_tangelo('--config', json.dumps(config), timeout=3, terminate=True)
-    stderr = '\n'.join(stderr)
+    (_, _, stderr) = fixture.run_tangelo("--config", json.dumps(config), timeout=3, terminate=True)
+    stderr = "\n".join(stderr)
 
-    assert 'Plugin has value server.TestConstant True' in stderr
+    assert "Plugin has value server.TestConstant True" in stderr
 
 
 def test_plugin_single_file():
-    config = {'plugins': [{
-        'name': 'pythonfile',
-        'path': 'tests/plugins/pythonfile'
+    config = {"plugins": [{
+        "name": "pythonfile",
+        "path": "tests/plugins/pythonfile"
     }]}
-    (_, _, stderr) = fixture.run_tangelo('--config', json.dumps(config), timeout=3, terminate=True)
-    stderr = '\n'.join(stderr)
+    (_, _, stderr) = fixture.run_tangelo("--config", json.dumps(config), timeout=3, terminate=True)
+    stderr = "\n".join(stderr)
 
-    assert 'Python file plugin' in stderr
+    assert "Python file plugin" in stderr
 
 
 def test_plugin_order_good():
-    config = {'plugins': [{
-        'name': 'moduletest',
-        'path': 'tests/plugins/moduletest'
+    config = {"plugins": [{
+        "name": "moduletest",
+        "path": "tests/plugins/moduletest"
     }, {
-        'name': 'pluginorder',
-        'path': 'tests/plugins/pluginorder'
+        "name": "pluginorder",
+        "path": "tests/plugins/pluginorder"
     }]}
-    (_, _, stderr) = fixture.run_tangelo('--config', json.dumps(config), timeout=3, terminate=True)
-    stderr = '\n'.join(stderr)
+    (_, _, stderr) = fixture.run_tangelo("--config", json.dumps(config), timeout=3, terminate=True)
+    stderr = "\n".join(stderr)
 
-    assert 'Plugin can reference tangelo.plugin.moduletest True' in stderr
+    assert "Plugin can reference tangelo.plugin.moduletest True" in stderr
 
 
 def test_plugin_order_bad():
-    config = {'plugins': [{
-        'name': 'pluginorder',
-        'path': 'tests/plugins/pluginorder'
+    config = {"plugins": [{
+        "name": "pluginorder",
+        "path": "tests/plugins/pluginorder"
     }, {
-        'name': 'moduletest',
-        'path': 'tests/plugins/moduletest'
+        "name": "moduletest",
+        "path": "tests/plugins/moduletest"
     }]}
-    (_, _, stderr) = fixture.run_tangelo('--config', json.dumps(config), timeout=3, terminate=True)
+    (_, _, stderr) = fixture.run_tangelo("--config", json.dumps(config), timeout=3, terminate=True)
     stderr = "\n".join(stderr)
 
     assert "Plugin pluginorder failed to load" in stderr
