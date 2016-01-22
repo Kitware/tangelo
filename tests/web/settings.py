07034e25419b0bca9cd07828acf14a03bdd0726b
Roni Choudhury <roni.choudhury@kitware.com>
2016-01-22 13:09:59 -0500

    Clarify broken line

--- tests/web/settings.py
+++ tests/web/settings.py
@@ -5,7 +5,6 @@ import tangelo
 # This service reports the value of cherrypy's thread pool setting
 def run(**kwargs):
     if kwargs.get('pool'):
-        tangelo.util.set_server_setting('server.thread_pool',
-                                 int(kwargs['pool']))
+        tangelo.util.set_server_setting('server.thread_pool', int(kwargs['pool']))
     response = 'pool="%r"' % cherrypy.config.get('server.thread_pool')
     return response
