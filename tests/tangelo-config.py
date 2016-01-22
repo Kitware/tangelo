d59f354e22de9b44a4e010d0d66ee6dac2a6b4ad
Roni Choudhury <roni.choudhury@kitware.com>
2016-01-22 11:50:31 -0500

    Use correct setting "thread_pool" in test

--- tests/tangelo-config.py
+++ tests/tangelo-config.py
@@ -40,7 +40,7 @@ def test_inline_config():
 
 def test_settings_config():
     def read_threadpool():
-        return requests.get(fixture.url("setting/server.threadpool"))
+        return requests.get(fixture.url("setting/server.thread_pool"))
 
     (_, _, stderr, r) = fixture.run_tangelo("-c", "tests/config/settings-config.yaml", "--host", fixture.host,
                                             "--port", fixture.port, "--root", "tests/web",
@@ -49,7 +49,7 @@ def test_settings_config():
     line = filter(lambda x: "->" in x, stderr)
 
     assert len(line) > 0
-    assert "\tserver.threadpool -> 1000" in line[0]
+    assert "\tserver.thread_pool -> 1000" in line[0]
 
     assert r.ok
     assert r.text == "1000"
